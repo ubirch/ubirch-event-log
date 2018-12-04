beac589067b7737bad8eff601a62d8ec49fd4eb6
derMicha <dermicha@ubirch.com>
2018-12-04 19:15:32 +0100

    added EventBase/EventMsgBase traits

--- src/main/scala/com/ubirch/models/Event.scala
+++ src/main/scala/com/ubirch/models/Event.scala
@@ -1,17 +1,30 @@
 package com.ubirch.models
 
-import java.util.{ Date, UUID }
+import java.util.{Date, UUID}
 
 import com.ubirch.services.cluster.ConnectionService
-import io.getquill.Embedded
+import io.getquill.{CassandraAsyncContext, Embedded, SnakeCase}
 import javax.inject._
 import org.json4s.JValue
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
-case class Event(id: UUID, serviceClass: String, category: String, event: JValue, eventTime: Date, eventTimeInfo: TimeInfo) extends Embedded
+trait EventBase {
+  val id: UUID
+  val serviceClass: String
+  val category: String
+  val eventTime: Date
+}
 
-case class EventLog(event: Event, signature: String, created: Date, updated: Date)
+trait EventMsgBase {
+  val event: EventBase
+  val signature: String
+}
+
+case class Event(id: UUID, serviceClass: String, category: String, event: JValue, eventTime: Date, eventTimeInfo: TimeInfo) extends Embedded with EventBase
+
+//TODO check whether we could use org.joda.time.DateTime instead of Date
+case class EventLog(event: Event, signature: String, created: Date, updated: Date) extends EventMsgBase
 
 trait EventLogQueries extends TablePointer[EventLog] with CustomEncodings[EventLog] {
 
@@ -19,11 +32,11 @@ trait EventLogQueries extends TablePointer[EventLog] with CustomEncodings[EventL
 
   //These represent query descriptions only
 
-  implicit val eventSchemaMeta = schemaMeta[EventLog]("events")
+  implicit val eventSchemaMeta: db.SchemaMeta[EventLog] = schemaMeta[EventLog]("events")
 
-  def selectAllQ = quote(query[EventLog])
+  def selectAllQ: db.Quoted[db.EntityQuery[EventLog]] = quote(query[EventLog])
 
-  def insertQ(eventlog: EventLog) = quote {
+  def insertQ(eventlog: EventLog): db.Quoted[db.Insert[EventLog]] = quote {
     query[EventLog].insert(lift(eventlog))
   }
 
@@ -32,14 +45,14 @@ trait EventLogQueries extends TablePointer[EventLog] with CustomEncodings[EventL
 @Singleton
 class Events @Inject() (val connectionService: ConnectionService)(implicit ec: ExecutionContext) extends EventLogQueries {
 
-  val db = connectionService.context
+  val db: CassandraAsyncContext[SnakeCase.type] = connectionService.context
 
   import db._
 
   //These actually run the queries.
 
-  def selectAll = run(selectAllQ)
+  def selectAll: Future[List[EventLog]] = run(selectAllQ)
 
-  def insert(eventlog: EventLog) = insertQ(eventlog)
+  def insert(eventlog: EventLog): db.Quoted[db.Insert[EventLog]] = insertQ(eventlog)
 
 }
