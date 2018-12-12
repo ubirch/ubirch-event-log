ff2f8ac0d7968dc9926e086522a4d543a712a7cb
Carlos Sanchez <caaugustoss@gmail.com>
2018-12-12 02:14:17 +0100

    UP-382 implement Kafka consumer #comment Adding Kafka Consumer Foundations so that it can be integrated with the store part of the system

---
+++ src/main/scala/com/ubirch/services/kafka/Configs.scala
@@ -0,0 +1,57 @@
+package com.ubirch.services.kafka
+
+import org.apache.kafka.clients.consumer.{ ConsumerConfig, OffsetResetStrategy }
+import org.apache.kafka.common.requests.IsolationLevel
+
+trait Configs {
+
+  val props: Map[String, AnyRef]
+
+  def withProperty(key: String, value: AnyRef): Configs = {
+    new Configs {
+      override val props: Map[String, AnyRef] = props + (key -> value)
+    }
+
+  }
+
+  def withConf(config: Configs): Configs = {
+    new Configs {
+      override val props: Map[String, AnyRef] = props ++ config.props
+    }
+  }
+
+}
+
+object Configs {
+
+  def apply[K, V](bootstrapServers: String = "localhost:9092",
+    groupId: String,
+    enableAutoCommit: Boolean = true,
+    autoCommitInterval: Int = 1000,
+    sessionTimeoutMs: Int = 10000,
+    maxPartitionFetchBytes: Int = ConsumerConfig.DEFAULT_MAX_PARTITION_FETCH_BYTES,
+    maxPollRecords: Int = 500,
+    maxPollInterval: Int = 300000,
+    maxMetaDataAge: Long = 300000,
+    autoOffsetReset: OffsetResetStrategy = OffsetResetStrategy.LATEST,
+    isolationLevel: IsolationLevel = IsolationLevel.READ_UNCOMMITTED): Configs = {
+
+    new Configs {
+      override val props: Map[String, AnyRef] = {
+        Map[String, AnyRef](
+          ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> bootstrapServers,
+          ConsumerConfig.GROUP_ID_CONFIG -> groupId,
+          ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> enableAutoCommit.toString,
+          ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG -> autoCommitInterval.toString,
+          ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG -> sessionTimeoutMs.toString,
+          ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG -> maxPartitionFetchBytes.toString,
+          ConsumerConfig.MAX_POLL_RECORDS_CONFIG -> maxPollRecords.toString,
+          ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG -> maxPollInterval.toString,
+          ConsumerConfig.METADATA_MAX_AGE_CONFIG -> maxMetaDataAge.toString,
+          ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> autoOffsetReset.toString.toLowerCase,
+          ConsumerConfig.ISOLATION_LEVEL_CONFIG -> isolationLevel.toString.toLowerCase())
+      }
+    }
+
+  }
+}
---
+++ src/main/scala/com/ubirch/services/kafka/MessageEnvelope.scala
@@ -0,0 +1,29 @@
+package com.ubirch.services.kafka
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.Header
+import org.apache.kafka.common.header.internals.RecordHeader
+
+import scala.collection.JavaConverters._
+
+case class MessageEnvelope[T](payload: T, headers: Map[String, String])
+
+object MessageEnvelope {
+  def apply[T](payload: T): MessageEnvelope[T] = {
+    MessageEnvelope(payload, Map())
+  }
+
+  def fromRecord[T](consumerRecord: ConsumerRecord[String, T]): MessageEnvelope[T] = {
+    MessageEnvelope(
+      consumerRecord.value(),
+      consumerRecord.headers().asScala.map(h ⇒ h.key() -> new String(h.value())).toMap)
+  }
+
+  def toRecord[T](topic: String, key: String, messageEnvelope: MessageEnvelope[T]): ProducerRecord[String, T] = {
+    val kafkaHeaders: Iterable[Header] = messageEnvelope.headers.map {
+      case (k: String, v: Any) ⇒ new RecordHeader(k, v.getBytes)
+    }
+    new ProducerRecord[String, T](topic, null, key, messageEnvelope.payload, kafkaHeaders.asJava)
+  }
+}
