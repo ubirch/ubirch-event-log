dc8d6703749d51cd80d21772ff0b258f4e82de78
Carlos Sanchez <caaugustoss@gmail.com>
2018-12-12 19:26:51 +0100

    UP-382 implement Kafka consumer #comment Adding ConfPaths object to keep references to the application conf

---
+++ src/main/scala/com/ubirch/ConfPaths.scala
@@ -0,0 +1,9 @@
+package com.ubirch
+
+object ConfPaths {
+
+  final val TOPIC_PATH = "eventLog.kafkaConsumer.topic"
+  final val GROUP_ID_PATH = "eventLog.kafkaConsumer.groupId"
+  final val GRACEFUL_TIMEOUT_PATH = "eventLog.kafkaConsumer.gracefulTimeout"
+
+}
