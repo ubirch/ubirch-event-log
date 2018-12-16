f109f150ba8607a6f4be688be97d87d42efb90c2
Carlos Sanchez <caaugustoss@gmail.com>
2018-12-16 23:27:24 +0100

    UP-382 implement Kafka consumer #comment Organizing some assets.

---
+++ src/main/scala/com/ubirch/services/kafka/KafkaConsumerBase.scala
@@ -0,0 +1,35 @@
+package com.ubirch.services.kafka
+
+import org.apache.kafka.clients.consumer.{ ConsumerRecords, KafkaConsumer ⇒ JKafkaConsumer }
+import org.apache.kafka.common.serialization.Deserializer
+
+import scala.collection.JavaConverters._
+import scala.util.Try
+
+trait KafkaConsumerBase[K, V] {
+
+  var consumer: JKafkaConsumer[K, V] = _
+
+  val topic: String
+
+  val keyDeserializer: Deserializer[K]
+
+  val valueDeserializer: Deserializer[V]
+
+  def createConsumer(props: Map[String, AnyRef]): JKafkaConsumer[K, V] = {
+    keyDeserializer.configure(props.asJava, true)
+    valueDeserializer.configure(props.asJava, false)
+    consumer = new JKafkaConsumer[K, V](props.asJava, keyDeserializer, valueDeserializer)
+    consumer
+  }
+
+  def subscribe(): this.type = {
+    consumer.subscribe(List(topic).asJavaCollection)
+    this
+  }
+
+  def pollRecords: Try[ConsumerRecords[K, V]] = {
+    Try(consumer.poll(java.time.Duration.ofSeconds(1)))
+  }
+
+}
