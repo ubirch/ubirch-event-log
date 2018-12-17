782d755d64e29a5e7cac706a8594af59c5577faa
Carlos Sanchez <caaugustoss@gmail.com>
2018-12-17 23:30:31 +0100

    UP-382 implement Kafka consumer #comment Adding conf per env and adding new test

---
+++ src/test/scala/com/ubirch/services/kafka/ExecutorSpec.scala
@@ -0,0 +1,40 @@
+package com.ubirch.services.kafka
+
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.mockito.MockitoSugar
+
+import scala.language.{ implicitConversions, postfixOps }
+
+class ExecutorSpec extends TestBase with MockitoSugar with LazyLogging {
+
+  "ExecutorSpec" must {
+
+    "pass the same result as it comes in" in {
+
+      val executor = new Executor[String, String] {
+        override def apply(v1: String): String = v1
+      }
+
+      assert(executor("Hello World") == "Hello World")
+
+    }
+
+    "compose two executors" in {
+
+      val executor1 = new Executor[String, String] {
+        override def apply(v1: String): String = v1.toLowerCase()
+      }
+
+      val executor2 = new Executor[String, String] {
+        override def apply(v1: String): String = v1.reverse
+      }
+
+      val composed = executor1 andThen executor2
+
+      assert(composed("Hello World") == "dlrow olleh")
+
+    }
+
+  }
+
+}
