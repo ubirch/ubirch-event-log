47b3d88b5883a203134f715ced3bc0e68e1f02cc
Carlos Sanchez <caaugustoss@gmail.com>
2018-12-19 22:35:44 +0100

    UP-382 implement Kafka consumer #comment Adding Port and Name Gens for Tests

---
+++ src/test/scala/com/ubirch/services/kafka/Givers.scala
@@ -0,0 +1,33 @@
+package com.ubirch.services.kafka
+
+import java.util.UUID
+
+object PortGiver {
+
+  private var kafkaPort = 9092
+
+  def giveMeKafkaPort: Int = {
+    this.synchronized {
+      kafkaPort = kafkaPort + 1
+      kafkaPort
+    }
+  }
+
+  private var zooKeeperPort = 6001
+
+  def giveMeZookeeperPort: Int = {
+    this.synchronized {
+      zooKeeperPort = zooKeeperPort + 1
+      zooKeeperPort
+    }
+  }
+
+}
+
+object NameGiver {
+
+  def giveMeATopicName = "com.ubirch.eventlog_" + UUID.randomUUID()
+
+  def giveMeAThreadName = "my_eventlog_thread" + UUID.randomUUID()
+
+}
