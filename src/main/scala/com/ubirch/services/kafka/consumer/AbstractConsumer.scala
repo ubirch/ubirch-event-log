8368e4add4bdabfdc69df6aa44fde7121d9c1f64
Carlos Sanchez <caaugustoss@gmail.com>
2018-12-20 19:21:25 +0100

    UP-382 implement Kafka consumer #comment Splitting file for better reading

--- src/main/scala/com/ubirch/services/kafka/consumer/AbstractConsumer.scala
+++ src/main/scala/com/ubirch/services/kafka/consumer/AbstractConsumer.scala
@@ -6,42 +6,12 @@ import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.models.Error
 import com.ubirch.util.Exceptions.{ EmptyValueException, ParsingIntoEventLogException, StoringIntoEventLogException }
 import com.ubirch.util.ShutdownableThread
-import org.apache.kafka.clients.consumer.ConsumerConfig
 
 import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.concurrent.{ Await, Future }
 import scala.language.postfixOps
 import scala.util.Try
-
-trait ConfigBaseHelpers {
-
-  private var _props: Map[String, AnyRef] = Map.empty
-
-  def props = _props
-
-  def withProps(ps: Map[String, AnyRef]): this.type = {
-    _props = ps
-    this
-  }
-
-  private var _topic: String = ""
-
-  def topic = _topic
-
-  def withTopic(value: String): this.type = {
-    _topic = value
-    this
-  }
-
-  def isAutoCommit: Boolean = props
-    .filterKeys(x ⇒ ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG == x)
-    .exists {
-      case (_, b) ⇒
-        b.toString.toBoolean
-    }
-
-}
 
 abstract class AbstractConsumer[K, V, R](name: String)
     extends ShutdownableThread(name)
