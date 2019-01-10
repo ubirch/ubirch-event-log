f47943350ac1b973ac3d036d2700dc71f696e2ea
Carlos Sanchez <caaugustoss@gmail.com>
2019-01-10 22:27:41 +0100

    UP-382 implement Kafka consumer #comment Adding EventLogSpec test for Global/Integration Tests

--- src/test/scala/com/ubirch/EventLogSpec.scala
+++ src/test/scala/com/ubirch/EventLogSpec.scala
@@ -1,5 +1,7 @@
 package com.ubirch
 
+import java.util.Date
+
 import com.github.nosan.embedded.cassandra.cql.CqlScript
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.models.Events
@@ -12,13 +14,20 @@ import org.apache.kafka.clients.consumer.OffsetResetStrategy
 
 import scala.language.{ implicitConversions, postfixOps }
 
-class StringConsumerSpec extends TestBase with EmbeddedCassandra with LazyLogging {
+class EventLogSpec extends TestBase with EmbeddedCassandra with LazyLogging {
 
-  "StringConsumerSpec" must {
+  "EventLogSpec" must {
 
-    "run Executors successfully and complete expected promise" in {
+    "consume message and store it in cassandra" in {
 
       implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
+
+      val configs = Configs(
+        bootstrapServers = "localhost:" + config.kafkaPort,
+        groupId = "My_Group_ID",
+        autoOffsetReset =
+          OffsetResetStrategy.EARLIEST
+      )
 
       withRunningKafka {
 
@@ -32,12 +41,6 @@ class StringConsumerSpec extends TestBase with EmbeddedCassandra with LazyLoggin
         publishStringMessageToKafka(topic, entityAsString1)
 
         //Consumer
-        val configs = Configs(
-          bootstrapServers = "localhost:" + config.kafkaPort,
-          groupId = "My_Group_ID",
-          autoOffsetReset =
-            OffsetResetStrategy.EARLIEST
-        )
         val consumer = get[StringConsumer].withProps(configs)
 
         consumer.startPolling()
@@ -55,6 +58,7 @@ class StringConsumerSpec extends TestBase with EmbeddedCassandra with LazyLoggin
         assert(res1.nonEmpty)
         assert(res1.headOption == Option(entity1))
 
+        //Next Message
         val entity2 = Entities.Events.eventExample()
         val entityAsString2 = Entities.Events.eventExampleAsString(entity2)
 
@@ -73,6 +77,85 @@ class StringConsumerSpec extends TestBase with EmbeddedCassandra with LazyLoggin
 
     }
 
+    "not insert message twice with same id" in {
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
+
+      val configs = Configs(
+        bootstrapServers = "localhost:" + config.kafkaPort,
+        groupId = "My_Group_ID",
+        autoOffsetReset =
+          OffsetResetStrategy.EARLIEST
+      )
+
+      withRunningKafka {
+
+        import InjectorHelper._
+
+        val topic = "com.ubirch.eventlog"
+
+        val entity1 = Entities.Events.eventExample()
+        val entityAsString1 = Entities.Events.eventExampleAsString(entity1)
+
+        publishStringMessageToKafka(topic, entityAsString1)
+
+        //Consumer
+        val consumer = get[StringConsumer].withProps(configs)
+
+        consumer.startPolling()
+        //Consumer
+
+        Thread.sleep(5000)
+
+        //Read Events
+        val events = get[Events]
+        def res = events.selectAll
+        //Read
+
+        val res1 = await(res)
+
+        assert(res1.nonEmpty)
+        assert(res1.contains(entity1))
+
+        //Next Message
+
+        publishStringMessageToKafka(topic, entityAsString1)
+
+        Thread.sleep(5000) //Wait for next consumption
+
+        val res2 = await(res)
+
+        assert(res2.nonEmpty)
+
+        assert(res2.contains(entity1))
+
+        assert(res2.size == 1)
+
+        //Next Message with same id but different stuff inside
+
+        val entity1Modified = entity1.copy(signature = "This is another signature", created = new Date)
+        val entityAsString1Modified = Entities.Events.eventExampleAsString(entity1Modified)
+
+        publishStringMessageToKafka(topic, entityAsString1Modified)
+
+        Thread.sleep(5000) //Wait for next consumption
+
+        val res3 = await(res)
+
+        assert(res3.nonEmpty)
+
+        assert(!res3.contains(entity1))
+
+        assert(res3.size == 1)
+
+      }
+    }
+
+  }
+
+  override protected def beforeEach(): Unit = {
+    cassandra.executeScripts(CqlScript.statements("TRUNCATE events;"))
+
+    Thread.sleep(5000)
   }
 
   override protected def beforeAll(): Unit = {
