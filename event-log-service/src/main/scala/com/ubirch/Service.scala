4ba001da170010ee903e32d0259d962d0d2fa99a
Carlos Sanchez <caaugustoss@gmail.com>
2019-01-18 22:46:33 +0100

    UP-382 implement Kafka consumer #comment Refactoring project to support multi-modules // Improving Poms. Step 8.

--- event-log-service/src/main/scala/com/ubirch/Service.scala
+++ event-log-service/src/main/scala/com/ubirch/Service.scala
@@ -1,11 +1,10 @@
 package com.ubirch
 
 import com.typesafe.scalalogging.LazyLogging
-import com.ubirch.services.execution.Execution
 import com.ubirch.services.kafka.consumer.StringConsumer
 import com.ubirch.util.Boot
 
-object Service extends Boot with LazyLogging with Execution {
+object Service extends Boot with LazyLogging {
 
   def main(args: Array[String]): Unit = {
 
