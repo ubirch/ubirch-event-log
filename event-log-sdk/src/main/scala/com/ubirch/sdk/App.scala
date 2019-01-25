0a57c5e50e17f0b79045925b10b2325fe0e4db77
Carlos Sanchez <caaugustoss@gmail.com>
2019-01-25 05:26:29 +0100

    UP-381 implemente a event log service client #comment Adding tests

--- event-log-sdk/src/main/scala/com/ubirch/sdk/App.scala
+++ event-log-sdk/src/main/scala/com/ubirch/sdk/App.scala
@@ -9,16 +9,46 @@ object App extends EventLogging {
     case class Hello(name: String)
 
     //From JSValue
+    val log1 = log(ToJson(Hello("Hola")).get, "My Category")
 
-    def log1 = log(ToJson(Hello("Hola")).get, "My Category")
+    val log2 = log(ToJson(Hello("Como estas")).get, "My another Category")
 
-    log1.commit
+    //Let's unite them in order first in first out
+    val log1_2 = log1 +> log2
 
-    val log2 = log(Hello("Hello"), "My Category")
-
-    val log1_2 = log1 -> log2
-
+    //Let's actually commit it
     log1_2.commit
+
+
+    //Another Log From A Case Class
+    val log3 = log(Hello("Hola"), "Category")
+
+    val log4 = log(Hello("Como estas"))
+
+    //Let's unite them in order first in last out
+    val log3_4 = log3 <+ log4
+
+    //Let's actually commit it
+    log3_4.commit
+
+    //Wanna have list of events and fold it
+
+    val foldedLogs = List(
+      log(Hello("Hello")),
+      log(Hello("Hallo")),
+      log(Hello("Hola"))
+    )
+
+    foldedLogs.commit
+
+
+    //By default the service class is the class extending or mixing the EventLogging trait
+    //But you can also change it
+
+    val log5 = log(Hello("Buenos Dias"), "THIS_IS_MY_CUSTOMIZED_SERVICE_CLASS", "Category")
+
+    log5.commit
+
 
   }
 
--- event-log-sdk/src/test/scala/com/ubirch/EventLoggingSpec.scala
+++ event-log-sdk/src/test/scala/com/ubirch/EventLoggingSpec.scala
@@ -2,8 +2,9 @@ package com.ubirch
 
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.sdk.EventLogging
-import com.ubirch.services.kafka.producer.{ Configs, StringProducer }
+import com.ubirch.services.kafka.producer.{Configs, StringProducer}
 import com.ubirch.util.Implicits.configsToProps
+import com.ubirch.util.ToJson
 import net.manub.embeddedkafka.EmbeddedKafkaConfig
 import org.scalatest.mockito.MockitoSugar
 
@@ -13,7 +14,7 @@ class EventLoggingSpec extends TestBase with MockitoSugar with LazyLogging {
 
   "EventLogging" must {
 
-    "not be created when props are empty" in {
+    "log message" in {
 
       implicit val kafKaConfig = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
 
@@ -30,6 +31,25 @@ class EventLoggingSpec extends TestBase with MockitoSugar with LazyLogging {
         val logged = logger.log(Hello("Hello")).commit
 
         consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe logged.toString
+
+        val logged2 = logger.log(ToJson("Hola").get).commit
+
+        consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe logged2.toString
+
+
+        val log1 = log(ToJson(Hello("Hola")).get, "My Category")
+
+        val log2 = log(ToJson(Hello("Como estas")).get, "My another Category")
+
+        //Let's unite them in order first in first out
+        val log1_2 = log1 +> log2
+
+        //Let's actually commit it
+        log1_2.commit
+
+        consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe log1.toString
+
+        consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe log2.toString
 
         getStringProducer.producer.close()
       }
