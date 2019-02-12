dbe9c869722941c12bb3de46d9309e36273767dd
derMicha <dermicha@ubirch.com>
2019-02-12 16:26:09 +0100

    deployment works now

--- helm-charts/deploy.sh
+++ helm-charts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 /keybase/team/ubirchdevops/bin/helm.sh dev delete event-log-service --purge
-/keybase/team/ubirchdevops/bin/helm.sh dev install event-log-service --namespace core-dev --name event-log-service --debug -f event-log-service/values-dev.yaml \
-   --set Values.build.number=20190212-dev
+/keybase/team/ubirchdevops/bin/helm.sh dev install \
+    event-log-service --namespace core-dev \
+    --name event-log-service --debug \
+    -f event-log-service/values-dev.yaml \
+    --set image.tag=$1
