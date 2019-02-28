861e707d6bd4aa1f9d6d46bf0d651dff1d502e92
derMicha <dermicha@ubirch.com>
2019-02-28 18:31:58 +0100

    added simple test function

--- event-log-chainer/src/main/scala/com/ubirch/ChainerService.scala
+++ event-log-chainer/src/main/scala/com/ubirch/ChainerService.scala
@@ -4,6 +4,11 @@ import com.ubirch.models.Node
 
 object ChainerService extends App {
 
+  def getEmptyNodeVal = {
+    val uuid =java.util.UUID.randomUUID().toString
+    s"emptyNode_$uuid"
+  }
+
   // This is an example that shows how we
   // - Take a list of seeds and turn them into empty nodes -no left or right nodes-
   // - Balance the list to have an even number of nodes.
@@ -12,7 +17,9 @@ object ChainerService extends App {
   // - nodes to later have a node of these nodes.
 
   val node = Node.seeds("a", "b", "c")
-    .balanceRightWithEmpty("d")
+    .balanceRightWithEmpty(getEmptyNodeVal)
     .join((a, b) => a + b)
+
+  println(node)
 
 }
