85b270fd3c6aaf184d5104c7f8e912743127230f
Carlos Sanchez <caaugustoss@gmail.com>
2019-03-06 03:38:04 +0100

    UP-486 Extract Kafka Consumer to Independent Artifact Adding more tests to consumer runner

--- event-log-kafka/src/test/scala/com/ubirch/services/ConsumerRunnerSpec.scala
+++ event-log-kafka/src/test/scala/com/ubirch/services/ConsumerRunnerSpec.scala
@@ -107,179 +107,246 @@ class ConsumerRunnerSpec extends TestBase {
 
     }
 
-  }
+    "consumer 100 entities successfully" in {
 
-  "consumer 100 entities successfully" in {
+      val maxEntities = 100
+      val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
+      val counter = new CountDownLatch(maxEntities)
 
-    val maxEntities = 100
-    val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
-    val counter = new CountDownLatch(maxEntities)
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
 
-    implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
+      withRunningKafka {
 
-    withRunningKafka {
+        val topic = NameGiver.giveMeATopicName
 
-      val topic = NameGiver.giveMeATopicName
-
-      val messages = (1 to maxEntities).map(i => "Hello " + i).toList
-      messages.foreach { m =>
-        publishStringMessageToKafka(topic, m)
-      }
-
-      val configs = Configs(
-        bootstrapServers = "localhost:" + config.kafkaPort,
-        groupId = "My_Group_ID",
-        autoOffsetReset = OffsetResetStrategy.EARLIEST
-      )
-
-      def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
-        override val id: UUID = UUID.randomUUID()
-        override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
-      }
-
-      val consumer = new ConsumerRunner[String, String]("cr-4") {
-        override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
-
-        override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
-          futureMessages += consumerRecord.value()
-          counter.countDown()
-          Future.successful(processResult(consumerRecord))
+        val messages = (1 to maxEntities).map(i => "Hello " + i).toList
+        messages.foreach { m =>
+          publishStringMessageToKafka(topic, m)
         }
+
+        val configs = Configs(
+          bootstrapServers = "localhost:" + config.kafkaPort,
+          groupId = "My_Group_ID",
+          autoOffsetReset = OffsetResetStrategy.EARLIEST
+        )
+
+        def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
+          override val id: UUID = UUID.randomUUID()
+          override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
+        }
+
+        val consumer = new ConsumerRunner[String, String]("cr-4") {
+          override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+
+          override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
+            futureMessages += consumerRecord.value()
+            counter.countDown()
+            Future.successful(processResult(consumerRecord))
+          }
+        }
+
+        consumer.setKeyDeserializer(Some(new StringDeserializer()))
+        consumer.setValueDeserializer(Some(new StringDeserializer()))
+        consumer.setTopics(Set(topic))
+        consumer.setProps(configs)
+        consumer.startPolling()
+
+        counter.await()
+
+        assert(futureMessages.size == maxEntities)
+        assert(messages == futureMessages.toList)
+
       }
-
-      consumer.setKeyDeserializer(Some(new StringDeserializer()))
-      consumer.setValueDeserializer(Some(new StringDeserializer()))
-      consumer.setTopics(Set(topic))
-      consumer.setProps(configs)
-      consumer.startPolling()
-
-      counter.await()
-
-      assert(futureMessages.size == maxEntities)
-      assert(messages == futureMessages.toList)
 
     }
 
-  }
+    "run an NeedForPauseException and pause and then unpause" in {
+      val maxEntities = 1
+      val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
+      val counter = new CountDownLatch(maxEntities)
 
-  "run an NeedForPauseException and pause and then unpause" in {
-    val maxEntities = 1
-    val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
-    val counter = new CountDownLatch(maxEntities)
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
 
-    implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
+      withRunningKafka {
 
-    withRunningKafka {
+        val topic = NameGiver.giveMeATopicName
 
-      val topic = NameGiver.giveMeATopicName
-
-      val messages = (1 to maxEntities).map(i => "Hello " + i).toList
-      messages.foreach { m =>
-        publishStringMessageToKafka(topic, m)
-      }
-
-      val configs = Configs(
-        bootstrapServers = "localhost:" + config.kafkaPort,
-        groupId = "My_Group_ID",
-        autoOffsetReset = OffsetResetStrategy.EARLIEST
-      )
-
-      def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
-        override val id: UUID = UUID.randomUUID()
-        override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
-      }
-
-      val consumer = new ConsumerRunner[String, String]("cr-5") {
-        override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
-
-        override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
-          futureMessages += consumerRecord.value()
-          counter.countDown()
-          Future.failed(NeedForPauseException("Need to pause", "yeah"))
+        val messages = (1 to maxEntities).map(i => "Hello " + i).toList
+        messages.foreach { m =>
+          publishStringMessageToKafka(topic, m)
         }
+
+        val configs = Configs(
+          bootstrapServers = "localhost:" + config.kafkaPort,
+          groupId = "My_Group_ID",
+          autoOffsetReset = OffsetResetStrategy.EARLIEST
+        )
+
+        def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
+          override val id: UUID = UUID.randomUUID()
+          override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
+        }
+
+        val consumer = new ConsumerRunner[String, String]("cr-5") {
+          override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+
+          override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
+            futureMessages += consumerRecord.value()
+            counter.countDown()
+            Future.failed(NeedForPauseException("Need to pause", "yeah"))
+          }
+        }
+
+        consumer.setKeyDeserializer(Some(new StringDeserializer()))
+        consumer.setValueDeserializer(Some(new StringDeserializer()))
+        consumer.setTopics(Set(topic))
+        consumer.setProps(configs)
+        consumer.startPolling()
+
+        counter.await()
+
+        assert(futureMessages.size == maxEntities)
+        assert(messages == futureMessages.toList)
+
+        Thread.sleep(2000)
+
+        assert(consumer.getPausedHistory.get() >= 1)
+
+        Thread.sleep(2000)
+
+        assert(consumer.getUnPausedHistory.get() >= 1)
+
       }
+    }
 
-      consumer.setKeyDeserializer(Some(new StringDeserializer()))
-      consumer.setValueDeserializer(Some(new StringDeserializer()))
-      consumer.setTopics(Set(topic))
-      consumer.setProps(configs)
-      consumer.startPolling()
+    "run an NeedForPauseException and pause and then unpause when throttling" in {
+      val maxEntities = 1
+      val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
+      val counter = new CountDownLatch(maxEntities)
 
-      counter.await()
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
 
-      assert(futureMessages.size == maxEntities)
-      assert(messages == futureMessages.toList)
+      withRunningKafka {
 
-      Thread.sleep(2000)
+        val topic = NameGiver.giveMeATopicName
 
-      assert(consumer.getPausedHistory.get() >= 1)
+        val messages = (1 to maxEntities).map(i => "Hello " + i).toList
+        messages.foreach { m =>
+          publishStringMessageToKafka(topic, m)
+        }
 
-      Thread.sleep(2000)
+        val configs = Configs(
+          bootstrapServers = "localhost:" + config.kafkaPort,
+          groupId = "My_Group_ID",
+          autoOffsetReset = OffsetResetStrategy.EARLIEST
+        )
 
-      assert(consumer.getUnPausedHistory.get() >= 1)
+        def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
+          override val id: UUID = UUID.randomUUID()
+          override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
+        }
+
+        val consumer = new ConsumerRunner[String, String]("cr-6") {
+          override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+
+          override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
+            futureMessages += consumerRecord.value()
+            counter.countDown()
+            Future.failed(NeedForPauseException("Need to pause", "yeah"))
+          }
+        }
+
+        consumer.setKeyDeserializer(Some(new StringDeserializer()))
+        consumer.setValueDeserializer(Some(new StringDeserializer()))
+        consumer.setTopics(Set(topic))
+        consumer.setProps(configs)
+        consumer.setDelaySingleRecord(10 millis)
+        consumer.setDelayRecords(1000 millis)
+
+        consumer.startPolling()
+
+        counter.await()
+
+        assert(futureMessages.size == maxEntities)
+        assert(messages == futureMessages.toList)
+
+        Thread.sleep(2000)
+
+        assert(consumer.getPausedHistory.get() >= 1)
+
+        Thread.sleep(2000)
+
+        assert(consumer.getUnPausedHistory.get() >= 1)
+
+      }
 
     }
-  }
 
-  "run an NeedForPauseException and pause and then unpause when throttling" in {
-    val maxEntities = 1
-    val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
-    val counter = new CountDownLatch(maxEntities)
+    "consume complete after error" in {
+      val maxEntities = 10
+      val futureMessages = scala.collection.mutable.ListBuffer.empty[String]
 
-    implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
 
-    withRunningKafka {
+      withRunningKafka {
 
-      val topic = NameGiver.giveMeATopicName
+        val topic = NameGiver.giveMeATopicName
 
-      val messages = (1 to maxEntities).map(i => "Hello " + i).toList
-      messages.foreach { m =>
-        publishStringMessageToKafka(topic, m)
-      }
-
-      val configs = Configs(
-        bootstrapServers = "localhost:" + config.kafkaPort,
-        groupId = "My_Group_ID",
-        autoOffsetReset = OffsetResetStrategy.EARLIEST
-      )
-
-      def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
-        override val id: UUID = UUID.randomUUID()
-        override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
-      }
-
-      val consumer = new ConsumerRunner[String, String]("cr-6") {
-        override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
-
-        override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
-          futureMessages += consumerRecord.value()
-          counter.countDown()
-          Future.failed(NeedForPauseException("Need to pause", "yeah"))
+        val messages = (1 to maxEntities).map(i => "Hello " + i).toList
+        messages.foreach { m =>
+          publishStringMessageToKafka(topic, m)
         }
+
+        val configs = Configs(
+          bootstrapServers = "localhost:" + config.kafkaPort,
+          groupId = "My_Group_ID",
+          autoOffsetReset = OffsetResetStrategy.EARLIEST
+        )
+
+        def processResult(_consumerRecord: ConsumerRecord[String, String]) = new ProcessResult[String, String] {
+          override val id: UUID = UUID.randomUUID()
+          override val consumerRecord: ConsumerRecord[String, String] = _consumerRecord
+        }
+
+        val doErrorOn: Int = {
+          val start = 1
+          val end = maxEntities
+          val rnd = new scala.util.Random
+          start + rnd.nextInt((end - start) + 1)
+        }
+
+        var alreadyFailed = false
+        var current = 1
+
+        val consumer = new ConsumerRunner[String, String]("cr-5") {
+          override implicit def ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+
+          override def process(consumerRecord: ConsumerRecord[String, String]): Future[ProcessResult[String, String]] = {
+            current = current + 1
+            if (current == doErrorOn && !alreadyFailed) {
+              futureMessages.clear()
+              alreadyFailed = true
+              Future.failed(NeedForPauseException("Need to pause", "yeah"))
+            } else {
+              futureMessages += consumerRecord.value()
+              Future.successful(processResult(consumerRecord))
+            }
+
+          }
+        }
+
+        consumer.setKeyDeserializer(Some(new StringDeserializer()))
+        consumer.setValueDeserializer(Some(new StringDeserializer()))
+        consumer.setTopics(Set(topic))
+        consumer.setProps(configs)
+        consumer.startPolling()
+
+        Thread.sleep(5000)
+
+        assert(futureMessages.toSet == messages.toSet)
+        assert(futureMessages.toSet.size == maxEntities)
+
       }
-
-      consumer.setKeyDeserializer(Some(new StringDeserializer()))
-      consumer.setValueDeserializer(Some(new StringDeserializer()))
-      consumer.setTopics(Set(topic))
-      consumer.setProps(configs)
-      consumer.setDelaySingleRecord(10 millis)
-      consumer.setDelayRecords(1000 millis)
-
-      consumer.startPolling()
-
-      counter.await()
-
-      assert(futureMessages.size == maxEntities)
-      assert(messages == futureMessages.toList)
-
-      Thread.sleep(2000)
-
-      assert(consumer.getPausedHistory.get() >= 1)
-
-      Thread.sleep(2000)
-
-      assert(consumer.getUnPausedHistory.get() >= 1)
-
     }
 
   }
