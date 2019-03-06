8c351346b35c04d53306c6415fd141b2bdf34352
Carlos Sanchez <caaugustoss@gmail.com>
2019-03-06 16:09:41 +0100

    UP-486 Extract Kafka Consumer to Independent Adding some descriptions

--- event-log-kafka/src/main/scala/com/ubirch/util/VersionedLazyLogging.scala
+++ event-log-kafka/src/main/scala/com/ubirch/util/VersionedLazyLogging.scala
@@ -5,6 +5,9 @@ import java.util.concurrent.atomic.AtomicInteger
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
+/**
+  * Logging helper to add a version number to the instance upon logging.
+  */
 trait VersionedLazyLogging {
 
   @transient
