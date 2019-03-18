b8912b83dd412c0a000235bbb33166ff1c604291
Carlos Sanchez <caaugustoss@gmail.com>
2019-03-18 22:41:54 +0100

    Setting values for version 1.0.0

--- event-log-core/src/main/scala/com/ubirch/services/kafka/consumer/ConsumerRunner.scala
+++ event-log-core/src/main/scala/com/ubirch/services/kafka/consumer/ConsumerRunner.scala
@@ -426,7 +426,7 @@ abstract class ConsumerRunner[K, V](name: String)
         postCommitCallback.run(partitionRecordsSize)
       } catch {
         case e: TimeoutException =>
-          throw CommitTimeoutException("Commit timed out", commitFunc, e)
+          throw CommitTimeoutException("Commit timed out", () => commitFunc(), e)
         case e: Throwable =>
           throw e
       }
