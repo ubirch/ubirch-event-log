d4eca0a471bb85c0eb61759c1b4ae0913d44ff43
Carlos Sanchez <caaugustoss@gmail.com>
2019-03-22 02:32:12 +0100

    #UP-504 Ubirch Protocol Adapter #comment Wiring a couple of components together for the adapter

--- ubirch-protocol-adapter/src/main/scala/com/ubirch/services/AdapterServiceBinder.scala
+++ ubirch-protocol-adapter/src/main/scala/com/ubirch/services/AdapterServiceBinder.scala
@@ -2,9 +2,7 @@ package com.ubirch.services
 
 import com.google.inject.{ AbstractModule, Module }
 import com.typesafe.config.Config
-import com.ubirch.kafka.consumer.StringConsumer
 import com.ubirch.process.{ DefaultExecutorFamily, ExecutorFamily }
-import com.ubirch.services.cluster._
 import com.ubirch.services.config.ConfigProvider
 import com.ubirch.services.execution.ExecutionProvider
 import com.ubirch.services.kafka.consumer._
@@ -21,15 +19,13 @@ class AdapterServiceBinder extends AbstractModule {
   override def configure(): Unit = {
 
     bind(classOf[Lifecycle]).to(classOf[DefaultLifecycle])
-    bind(classOf[ClusterService]).to(classOf[DefaultClusterService])
-    bind(classOf[ConnectionService]).to(classOf[DefaultConnectionService])
     bind(classOf[JVMHook]).to(classOf[DefaultJVMHook])
     bind(classOf[ExecutorFamily]).to(classOf[DefaultExecutorFamily])
-    bind(classOf[StringConsumerRecordsManager]).to(classOf[UbirchProtocolAdapterConsumerRecordsManager])
+    bind(classOf[MessageEnvelopeConsumerRecordsManager]).to(classOf[DefaultMessageEnvelopeManager])
 
     bind(classOf[Config]).toProvider(classOf[ConfigProvider])
     bind(classOf[ExecutionContext]).toProvider(classOf[ExecutionProvider])
-    bind(classOf[StringConsumer]).toProvider(classOf[DefaultStringConsumer])
+    bind(classOf[MessageEnvelopeConsumer]).toProvider(classOf[DefaultMessageEnvelopeConsumer])
     bind(classOf[StringProducer]).toProvider(classOf[DefaultStringProducer])
 
   }
