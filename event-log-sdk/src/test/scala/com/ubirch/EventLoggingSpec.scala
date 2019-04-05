51d03a7cc96883206c78e4837ed44ffa9e12a657
Carlos Sanchez <caaugustoss@gmail.com>
2019-04-05 03:06:05 +0200

    Adding more tests

--- event-log-sdk/src/test/scala/com/ubirch/EventLoggingSpec.scala
+++ event-log-sdk/src/test/scala/com/ubirch/EventLoggingSpec.scala
@@ -27,17 +27,17 @@ class EventLoggingSpec extends TestBase with MockitoSugar with LazyLogging {
 
         setStringProducer(StringProducer(configs))
 
-        val logged = logger.log(Hello("Hello")).commit
+        val logged = logger.log(Hello("Hello")).withCustomerId("my customer id").commit
 
         consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe logged.toString
 
-        val logged2 = logger.log(EventLogJsonSupport.ToJson("Hola").get).commit
+        val logged2 = logger.log(EventLogJsonSupport.ToJson("Hola").get).withCustomerId("my customer id").commit
 
         consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe logged2.toString
 
-        val log1 = log(EventLogJsonSupport.ToJson(Hello("Hola")).get, "My Category")
+        val log1 = log(EventLogJsonSupport.ToJson(Hello("Hola")).get, "My Category").withCustomerId("my customer id")
 
-        val log2 = log(EventLogJsonSupport.ToJson(Hello("Como estas")).get, "My another Category")
+        val log2 = log(EventLogJsonSupport.ToJson(Hello("Como estas")).get, "My another Category").withCustomerId("my customer id 2")
 
         //Let's unite them in order first in first out
         val log1_2 = log1 +> log2
@@ -45,7 +45,11 @@ class EventLoggingSpec extends TestBase with MockitoSugar with LazyLogging {
         //Let's actually commit it
         log1_2.commit
 
-        val log3 = log(EventLogJsonSupport.ToJson(Hello("Como estas")).get, "my service class", "My another Category")
+        val log3 = log(
+          EventLogJsonSupport.ToJson(Hello("Como estas")).get,
+          "my service class",
+          "My another Category"
+        ).withCustomerId("my customer id")
 
         assert(log3.event == EventLogJsonSupport.ToJson(Hello("Como estas")).get)
         assert(log3.serviceClass == "my service class")
@@ -53,9 +57,11 @@ class EventLoggingSpec extends TestBase with MockitoSugar with LazyLogging {
 
         assert(log1.event == EventLogJsonSupport.ToJson(Hello("Hola")).get)
         assert(log1.category == "My Category")
+        assert(log1.customerId == "my customer id")
 
         assert(log2.event == EventLogJsonSupport.ToJson(Hello("Como estas")).get)
         assert(log2.category == "My another Category")
+        assert(log2.customerId == "my customer id 2")
 
         consumeFirstStringMessageFrom("com.ubirch.eventlog") mustBe log1.toString
 
