4cb3cec6abaabd8f55202941e5e62d1575a39012
derMicha <dermicha@ubirch.com>
2019-05-07 21:14:55 +0200

    config changed, deployment updated

--- helm-charts/deployAll.sh
+++ helm-charts/deployAll.sh
@@ -12,13 +12,17 @@ fi
 ENV=$1
 
 CUREVENTLOGTAG=201905052200-dev
-CUREVENTLOOKUPTAG=201905052015-dev
-CUREVENTADAPTERTAG=201905052213-dev
-CUREVENTDISPATCHERTAG=201905071016-dev
-CUREVENTCHAINERTAG=201905071343-dev
 
-#./deployK8SService.sh $ENV event-log-service $CUREVENTLOGTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-$ENV.yaml"
-#./deployK8SService.sh $ENV event-log-kafka-lookup $CUREVENTLOOKUPTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-kafka-lookup-$ENV.yaml"
-#./deployK8SService.sh $ENV ubirch-protocol-adapter $CUREVENTADAPTERTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-ubirch-protocol-adapter-$ENV.yaml"
-#./deployK8SService.sh $ENV event-log-dispatcher $CUREVENTDISPATCHERTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-dispatcher-$ENV.yaml"
+CUREVENTLOOKUPTAG=201905052015-dev
+
+CUREVENTADAPTERTAG=201905052213-dev
+
+CUREVENTDISPATCHERTAG=201905071422-dev
+
+CUREVENTCHAINERTAG=201905071858-dev
+
+./deployK8SService.sh $ENV event-log-service $CUREVENTLOGTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-$ENV.yaml"
+./deployK8SService.sh $ENV event-log-kafka-lookup $CUREVENTLOOKUPTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-kafka-lookup-$ENV.yaml"
+./deployK8SService.sh $ENV ubirch-protocol-adapter $CUREVENTADAPTERTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-ubirch-protocol-adapter-$ENV.yaml"
+./deployK8SService.sh $ENV event-log-dispatcher $CUREVENTDISPATCHERTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-dispatcher-$ENV.yaml"
 ./deployK8SService.sh $ENV event-log-chainer $CUREVENTCHAINERTAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-chainer-$ENV.yaml"
