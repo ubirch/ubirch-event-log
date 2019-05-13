e8f158e26e97fe7657d5b6d9c5f7e16d31d7ded0
Carlos Sanchez <caaugustoss@gmail.com>
2019-05-13 15:36:37 +0200

    Fixing lookup key category for tree event

--- event-log-core/src/main/scala/com/ubirch/models/LookupKey.scala
+++ event-log-core/src/main/scala/com/ubirch/models/LookupKey.scala
@@ -3,7 +3,7 @@ package com.ubirch.models
 case class LookupKey(name: String, category: String, key: String, value: Seq[String])
 
 object LookupKey {
-  final val SLAVE_TREE = "slave-tree-chainer"
+  final val SLAVE_TREE = "UBIRCH_SLAVE_TREE"
   final val SLAVE_TREE_ID = "slave-tree-id"
 
 }
