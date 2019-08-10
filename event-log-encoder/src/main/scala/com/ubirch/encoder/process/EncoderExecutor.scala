521ace98431b252e922b4444438e8fcd9fe6e78d
Carlos Sanchez <caaugustoss@gmail.com>
2019-08-10 13:03:28 +0200

    Testing new ec 2

--- event-log-encoder/src/main/scala/com/ubirch/encoder/process/EncoderExecutor.scala
+++ event-log-encoder/src/main/scala/com/ubirch/encoder/process/EncoderExecutor.scala
@@ -222,11 +222,10 @@ class EncoderExecutor @Inject() (
     }
 
   }
-
   val scheduler = monix.execution.Scheduler(executionContext)
 
   override def apply(v1: Vector[ConsumerRecord[String, Array[Byte]]]): Future[EncoderPipeData] = Future {
-    v1.foreach(x => Task.fork(run(x)).runAsync(scheduler))
+    v1.foreach(x => run(x).runAsync(scheduler))
     EncoderPipeData(v1, Vector.empty)
   }
 
