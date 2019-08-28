22fa1dfd5053296ac60f5b4b225d59f6547ec7e3
derMicha <dermicha@ubirch.com>
2019-08-28 14:59:54 +0200

    fixed deployment

--- helm-charts/deployAll.sh
+++ helm-charts/deployAll.sh
@@ -12,21 +12,13 @@ fi
 ENV=$1
 
 #CUR_TAG=116-fa46196
-CUR_TAG=latest
+if [[ "dev" == "$ENV" ]]; then
+    CUR_TAG=latest
+else
+#    CUR_TAG=stable
+    CUR_TAG=latest
 
-#CUR_EVENT_LOG_TAG=201906241004-dev
-#CUR_EVENT_LOOKUP_TAG=201906241004-dev
-#CUR_EVENT_DISPATCHER_TAG=201906241004-dev
-#CUR_EVENT_CHAINER_TAG=201906241004-dev
-#CUR_EVENT_CHAINER_MASTER_TAG=$CUR_EVENT_CHAINER_TAG
-#CUR_EVENT_ENCODER_TAG=201906241004-dev
-
-CUR_EVENT_LOG_TAG=${CUR_TAG}
-CUR_EVENT_LOOKUP_TAG=${CUR_TAG}
-CUR_EVENT_DISPATCHER_TAG=${CUR_TAG}
-CUR_EVENT_CHAINER_TAG=${CUR_TAG}
-CUR_EVENT_CHAINER_MASTER_TAG=${CUR_EVENT_CHAINER_TAG}
-CUR_EVENT_ENCODER_TAG=${CUR_TAG}
+fi
 
 #if [[ "dev" == "$ENV" ]]; then
 #     Insta
@@ -42,5 +34,5 @@ CUR_EVENT_ENCODER_TAG=${CUR_TAG}
 
 ./deployK8SService.sh $ENV event-log-encoder $CUR_EVENT_ENCODER_TAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-encoder-$ENV.yaml"
 ./deployK8SService.sh $ENV event-log-dispatcher $CUR_EVENT_DISPATCHER_TAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-dispatcher-$ENV.yaml"
-./deployK8SService.sh $ENV event-log-chainer $CUR_EVENT_CHAINER_TAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-chainer-$ENV.yaml"
-./deployK8SService.sh $ENV event-log-chainer-master $CUR_EVENT_CHAINER_MASTER_TAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-chainer-master-$ENV.yaml"
+./deployK8SService.sh $ENV event-log-chainer $CUR_TAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-chainer-$ENV.yaml"
+./deployK8SService.sh $ENV event-log-chainer-master $CUR_TAG "~/workspace/ubirch/_k8s/ubirch-kubernetes/19_event-log/values-event-log-chainer-master-$ENV.yaml"
