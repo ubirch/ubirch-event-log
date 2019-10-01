686286b48996ae28369f527dba0433730e3084e9
Carlos Sanchez <caaugustoss@gmail.com>
2019-10-01 12:16:59 +0200

    UP-865 #comment Adding next value to last master and previous to blockchains

--- event-log-kafka-lookup/src/main/scala/com/ubirch/lookup/models/VertexStruct.scala
+++ event-log-kafka-lookup/src/main/scala/com/ubirch/lookup/models/VertexStruct.scala
@@ -1,3 +1,14 @@
 package com.ubirch.lookup.models
 
-case class VertexStruct(label: String, properties: Map[String, String])
+case class VertexStruct(label: String, properties: Map[String, List[String]]) {
+  def addLabel(newLabel: String): VertexStruct = copy(label = newLabel)
+  def addProperties(newProperties: (String, List[String])*): VertexStruct =
+    copy(properties = this.properties ++ newProperties)
+
+  def addProperties(newProperties: Map[String, List[String]]): VertexStruct =
+    copy(properties = this.properties ++ newProperties)
+
+  def withProperties(newProperties: Map[String, List[String]]): VertexStruct =
+    copy(properties = newProperties)
+
+}
