de6e8e2fd1e01f098f2ec13948274c8a33981784
Carlos Sanchez <caaugustoss@gmail.com>
2019-10-01 15:51:32 +0200

    Revert "UP-865 #comment Adding more details to next and prev

--- event-log-kafka-lookup/src/main/scala/com/ubirch/lookup/models/VertexStruct.scala
+++ event-log-kafka-lookup/src/main/scala/com/ubirch/lookup/models/VertexStruct.scala
@@ -1,3 +1,18 @@
 package com.ubirch.lookup.models
 
-case class VertexStruct(label: String, properties: Map[String, String])
+import org.json4s.{ CustomSerializer, JObject }
+
+case class VertexStruct(label: String, properties: Map[String, String]) {
+
+  def addLabel(newLabel: String): VertexStruct = copy(label = newLabel)
+
+  def addProperties(newProperties: (String, String)*): VertexStruct =
+    copy(properties = this.properties ++ newProperties)
+
+  def addProperties(newProperties: Map[String, String]): VertexStruct =
+    copy(properties = this.properties ++ newProperties)
+
+  def withProperties(newProperties: Map[String, String]): VertexStruct =
+    copy(properties = newProperties)
+
+}
