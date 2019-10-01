5a8928ab46d3f6b4cde1c6e2cc806d88addcb492
Carlos Sanchez <caaugustoss@gmail.com>
2019-10-01 12:35:47 +0200

    Revert "UP-865 #comment Adding next value to last master and previous to blockchains"
    This reverts commit 849033e0

--- event-log-kafka-lookup/src/main/scala/com/ubirch/lookup/models/VertexStruct.scala
+++ event-log-kafka-lookup/src/main/scala/com/ubirch/lookup/models/VertexStruct.scala
@@ -1,14 +1,3 @@
 package com.ubirch.lookup.models
 
-case class VertexStruct(label: String, properties: Map[String, List[String]]) {
-  def addLabel(newLabel: String): VertexStruct = copy(label = newLabel)
-  def addProperties(newProperties: (String, List[String])*): VertexStruct =
-    copy(properties = this.properties ++ newProperties)
-
-  def addProperties(newProperties: Map[String, List[String]]): VertexStruct =
-    copy(properties = this.properties ++ newProperties)
-
-  def withProperties(newProperties: Map[String, List[String]]): VertexStruct =
-    copy(properties = newProperties)
-
-}
+case class VertexStruct(label: String, properties: Map[String, String])
