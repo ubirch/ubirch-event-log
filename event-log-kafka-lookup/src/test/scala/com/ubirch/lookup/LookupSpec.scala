d3ecd80390e90b95f2a9b40fcae004c10f8ab624
Carlos Sanchez <caaugustoss@gmail.com>
2019-11-05 13:40:53 +0100

    UP-1030 #comment Fixing lookup tests and adding one more

--- event-log-kafka-lookup/src/test/scala/com/ubirch/lookup/LookupSpec.scala
+++ event-log-kafka-lookup/src/test/scala/com/ubirch/lookup/LookupSpec.scala
@@ -44,13 +44,93 @@ class FakeFoundFinder @Inject() (cassandraFinder: CassandraFinder)(implicit val 
 
   def findUPP(value: String, queryType: QueryType): Future[Option[EventLogRow]] = cassandraFinder.findUPP(value, queryType)
 
-  def findAnchorsWithPathAsVertices(id: String): Future[(List[VertexStruct], List[VertexStruct])] = Future.successful((FakeFoundFinder.simplePath, FakeFoundFinder.blockchains))
+  def findAnchorsWithPathAsVertices(id: String): Future[(List[VertexStruct], List[VertexStruct])] =
+    Future.successful((FakeFoundFinder.simplePath, FakeFoundFinder.blockchains))
 
-  def findUpperAndLowerAsVertices(id: String): Future[(List[VertexStruct], List[VertexStruct], List[VertexStruct], List[VertexStruct])] = Future.successful((Nil, Nil, Nil, Nil))
+  def findUpperAndLowerAsVertices(id: String): Future[(List[VertexStruct], List[VertexStruct], List[VertexStruct], List[VertexStruct])] =
+    Future.successful((FakeFoundFinder.upperPath, FakeFoundFinder.upperBlockchains, FakeFoundFinder.lowerPath, FakeFoundFinder.lowerBlockchains))
 
 }
 
 object FakeFoundFinder {
+
+  val upperPath = List(
+    VertexStruct("UPP", properties = Map(
+      "next_hash" -> "eUhr08+42ZL5KTwV6+QrbZ+HLGKgxGLHONd9WuZ6bt/wf/hBdMxHjPa+3Kb8aUC9yvhGHotGXZrvQZ2wpSe2HQ==",
+      "signature" -> "bCWe6UOwYCJlZ5nEQmiQrqzW7PwMl2DSi1loPNwMmukD9lnTm7xACePNP4BzzWt3NSvqTqC/Nqka/GBDVXDZAg==",
+      "hash" -> "/gQVsIcokNP8DF9J8dAz7u7QxMzCODjmZLWIyCI93Zw8j6WQsy9QTX2HgpRL5S3nuO40vldfvWERLiE3axJiXQ==",
+      "prev_hash" -> "/gQVsIcokNP8DF9J8dAz7u7QxMzCODjmZLWIyCI93Zw8j6WQsy9QTX2HgpRL5S3nuO40vldfvWERLiE3axJiXQ==",
+      "type" -> "UPP"
+    )),
+    VertexStruct("UPP", properties = Map(
+      "next_hash" -> "18f8491f333b8ecf1bdb34db094284cb9416acc1ad04654617a05cdbb7686862f52f9963a681d11bb76e5bce680483f15f6916736855827bcf5fb11424d91fc6",
+      "signature" -> "YTKC9pYsKHaaxoz4g6r6MXgHq96eodAZWG5HaYHkPDX4hubgVtry36pypJORTGsYGujAfgtkhFyP1yYjdZZgDg==",
+      "hash" -> "eUhr08+42ZL5KTwV6+QrbZ+HLGKgxGLHONd9WuZ6bt/wf/hBdMxHjPa+3Kb8aUC9yvhGHotGXZrvQZ2wpSe2HQ==",
+      "prev_hash" -> "",
+      "type" -> "UPP"
+    )),
+    VertexStruct("SLAVE_TREE", properties = Map(
+      "next_hash" -> "ec0a1fbd0b5be6dd8ef1095293053e4d62b7348f37ed29beede57066900de3e38d1d3de8769bc0d1b29f8f4593dcdb97e1bcf7b8b3b8227542b826fe993634a6",
+      "hash" -> "18f8491f333b8ecf1bdb34db094284cb9416acc1ad04654617a05cdbb7686862f52f9963a681d11bb76e5bce680483f15f6916736855827bcf5fb11424d91fc6",
+      "prev_hash" -> "eUhr08+42ZL5KTwV6+QrbZ+HLGKgxGLHONd9WuZ6bt/wf/hBdMxHjPa+3Kb8aUC9yvhGHotGXZrvQZ2wpSe2HQ==",
+      "type" -> "SLAVE_TREE"
+    )),
+    VertexStruct("MASTER_TREE", properties = Map(
+      "next_hash" -> "Y9JAJGOZGLUQQFJOLEFVFUMTQILTZ9IKRPCFNEAGQEPRZPOWERJAQUQDCXHEUOCICGCSYCUBWDKBZ9999",
+      "hash" -> "ec0a1fbd0b5be6dd8ef1095293053e4d62b7348f37ed29beede57066900de3e38d1d3de8769bc0d1b29f8f4593dcdb97e1bcf7b8b3b8227542b826fe993634a6",
+      "prev_hash" -> "18f8491f333b8ecf1bdb34db094284cb9416acc1ad04654617a05cdbb7686862f52f9963a681d11bb76e5bce680483f15f6916736855827bcf5fb11424d91fc6",
+      "type" -> "MASTER_TREE"
+    ))
+  )
+
+  val upperBlockchains = List(
+    VertexStruct("PUBLIC_CHAIN", properties = Map(
+      "public_chain" -> "IOTA_TESTNET_IOTA_TESTNET_NETWORK",
+      "hash" -> "Y9JAJGOZGLUQQFJOLEFVFUMTQILTZ9IKRPCFNEAGQEPRZPOWERJAQUQDCXHEUOCICGCSYCUBWDKBZ9999",
+      "prev_hash" -> "ec0a1fbd0b5be6dd8ef1095293053e4d62b7348f37ed29beede57066900de3e38d1d3de8769bc0d1b29f8f4593dcdb97e1bcf7b8b3b8227542b826fe993634a6",
+      "type" -> "PUBLIC_CHAIN"
+    ))
+  )
+
+  val lowerPath = List(
+    VertexStruct(
+      "MASTER_TREE",
+      properties = Map(
+        "next_hash" -> "375f8683023272f4b31afd134411daddee94212d1f70f376c15ae23979207f26ece737f7ede575dd6b0b088cb154eb25a1ba09d35e51cf05b9bceb3e5f4e765e",
+        "hash" -> "0a4303cf15ade07b6987dd31aba5cf4a5e60058644176c22122e91e7b94e90e59ccd362dfc88d3e84baa5eef9aa7b9dd0cdf6a6151dceb7a9459bd090925b233",
+        "prev_hash" -> "",
+        "type" -> "MASTER_TREE"
+      )
+    ),
+    VertexStruct (
+      "MASTER_TREE",
+      properties = Map(
+        "next_hash" -> "ca96ca76c59753c27a100780d857bfdefe448ee16aa51aef6b3aa766bf28bb1e5bf355566124e830ef6641ef34f33a6b475641f4836dcbfe7b02c6edd6136eac",
+        "hash" -> "375f8683023272f4b31afd134411daddee94212d1f70f376c15ae23979207f26ece737f7ede575dd6b0b088cb154eb25a1ba09d35e51cf05b9bceb3e5f4e765e",
+        "prev_hash"  -> "0a4303cf15ade07b6987dd31aba5cf4a5e60058644176c22122e91e7b94e90e59ccd362dfc88d3e84baa5eef9aa7b9dd0cdf6a6151dceb7a9459bd090925b233",
+        "type" -> "MASTER_TREE"
+      )
+    ),
+    VertexStruct(
+      "MASTER_TREE",
+      properties = Map(
+        "next_hash"-> "MBCRQSNGBLVYHMJBKJHGFKFIDKVTFJHSMWUFPSCNOCUUIKQNML9H9KYQWBXUKCKNZIIOSUXPDORQZ9999",
+        "hash"-> "ca96ca76c59753c27a100780d857bfdefe448ee16aa51aef6b3aa766bf28bb1e5bf355566124e830ef6641ef34f33a6b475641f4836dcbfe7b02c6edd6136eac",
+        "prev_hash"-> "375f8683023272f4b31afd134411daddee94212d1f70f376c15ae23979207f26ece737f7ede575dd6b0b088cb154eb25a1ba09d35e51cf05b9bceb3e5f4e765e",
+        "type"-> "MASTER_TREE"
+      )
+    )
+  )
+
+  val lowerBlockchains = List(
+    VertexStruct("PUBLIC_CHAIN", properties = Map(
+      "public_chain" -> "IOTA_TESTNET_IOTA_TESTNET_NETWORK",
+      "hash" -> "MBCRQSNGBLVYHMJBKJHGFKFIDKVTFJHSMWUFPSCNOCUUIKQNML9H9KYQWBXUKCKNZIIOSUXPDORQZ9999",
+      "prev_hash" -> "ca96ca76c59753c27a100780d857bfdefe448ee16aa51aef6b3aa766bf28bb1e5bf355566124e830ef6641ef34f33a6b475641f4836dcbfe7b02c6edd6136eac",
+      "type" -> "PUBLIC_CHAIN"
+    ))
+  )
+
   val simplePath = List(
     VertexStruct("UPP", properties = Map(
       "next_hash" -> "eUhr08+42ZL5KTwV6+QrbZ+HLGKgxGLHONd9WuZ6bt/wf/hBdMxHjPa+3Kb8aUC9yvhGHotGXZrvQZ2wpSe2HQ==",
@@ -93,21 +173,21 @@ class LookupSpec extends TestBase with EmbeddedCassandra with LazyLogging {
 
   val insertEventSql: String =
     s"""
-      |INSERT INTO events (id, customer_id, service_class, category, event, event_time, year, month, day, hour, minute, second, milli, signature, nonce)
-      | VALUES ('c29tZSBieXRlcyEAAQIDnw==', 'customer_id', 'service_class', '${Values.UPP_CATEGORY}', '{
-      |   "hint":0,
-      |   "payload":"c29tZSBieXRlcyEAAQIDnw==",
-      |   "signature":"5aTelLQBerVT/vJiL2qjZCxWxqlfwT/BaID0zUVy7LyUC9nUdb02//aCiZ7xH1HglDqZ0Qqb7GyzF4jtBxfSBg==",
-      |   "signed":"lRKwjni1ymWXEeiBhcg+pwAOTQCwc29tZSBieXRlcyEAAQIDnw==",
-      |   "uuid":"8e78b5ca-6597-11e8-8185-c83ea7000e4d",
-      |   "version":34
-      |}', '2019-01-29T17:00:28.333Z', 2019, 5, 2, 19, 439, 16, 0, '0681D35827B17104A2AACCE5A08C4CD1BC8A5EF5EFF4A471D15976693CC0D6D67392F1CACAE63565D6E521D2325A998CDE00A2FEF5B65D0707F4158000EB6D05',
-      |'34376336396166392D336533382D343665652D393063332D616265313364383335353266');
+       |INSERT INTO events (id, customer_id, service_class, category, event, event_time, year, month, day, hour, minute, second, milli, signature, nonce)
+       | VALUES ('c29tZSBieXRlcyEAAQIDnw==', 'customer_id', 'service_class', '${Values.UPP_CATEGORY}', '{
+       |   "hint":0,
+       |   "payload":"c29tZSBieXRlcyEAAQIDnw==",
+       |   "signature":"5aTelLQBerVT/vJiL2qjZCxWxqlfwT/BaID0zUVy7LyUC9nUdb02//aCiZ7xH1HglDqZ0Qqb7GyzF4jtBxfSBg==",
+       |   "signed":"lRKwjni1ymWXEeiBhcg+pwAOTQCwc29tZSBieXRlcyEAAQIDnw==",
+       |   "uuid":"8e78b5ca-6597-11e8-8185-c83ea7000e4d",
+       |   "version":34
+       |}', '2019-01-29T17:00:28.333Z', 2019, 5, 2, 19, 439, 16, 0, '0681D35827B17104A2AACCE5A08C4CD1BC8A5EF5EFF4A471D15976693CC0D6D67392F1CACAE63565D6E521D2325A998CDE00A2FEF5B65D0707F4158000EB6D05',
+       |'34376336396166392D336533382D343665652D393063332D616265313364383335353266');
     """.stripMargin
 
   val insertLookupSql: String =
     s"""
-      |INSERT INTO lookups (name, category, key, value) VALUES ('${Signature.value}', '${Values.UPP_CATEGORY}', 'c29tZSBieXRlcyEAAQIDnw==', '5aTelLQBerVT/vJiL2qjZCxWxqlfwT/BaID0zUVy7LyUC9nUdb02//aCiZ7xH1HglDqZ0Qqb7GyzF4jtBxfSBg==');
+       |INSERT INTO lookups (name, category, key, value) VALUES ('${Signature.value}', '${Values.UPP_CATEGORY}', 'c29tZSBieXRlcyEAAQIDnw==', '5aTelLQBerVT/vJiL2qjZCxWxqlfwT/BaID0zUVy7LyUC9nUdb02//aCiZ7xH1HglDqZ0Qqb7GyzF4jtBxfSBg==');
     """.stripMargin
 
   implicit val se: StringSerializer = new StringSerializer
@@ -160,7 +240,20 @@ class LookupSpec extends TestBase with EmbeddedCassandra with LazyLogging {
         val value = "c29tZSBieXRlcyEAAQIDnw=="
         val queryType = Payload
         val queryDepth = ShortestPath
-        val pr = ProducerRecordHelper.toRecord(messageEnvelopeTopic, key, value, Map(QueryType.HEADER -> queryType.value, QueryDepth.HEADER -> queryDepth.value))
+        val responseForm = AnchorsWithPath
+        val blockchainInfo = Normal
+
+        val pr = ProducerRecordHelper.toRecord(
+          messageEnvelopeTopic,
+          key,
+          value,
+          Map(
+            QueryType.HEADER -> queryType.value,
+            QueryDepth.HEADER -> queryDepth.value,
+            ResponseForm.HEADER -> responseForm.value,
+            BlockchainInfo.HEADER -> blockchainInfo.value
+          )
+        )
         publishToKafka(pr)
 
         //Consumer
@@ -186,7 +279,12 @@ class LookupSpec extends TestBase with EmbeddedCassandra with LazyLogging {
             |}
           """.stripMargin
 
-        val expectedLookup = LookupResult.Found(key, queryType, LookupJsonSupport.getJValue(data), LookupExecutor.shortestPathAsJValue(FakeFoundFinder.simplePath, FakeFoundFinder.blockchains))
+        val expectedLookup = LookupResult.Found(
+          key = key,
+          queryType = queryType,
+          event = LookupJsonSupport.getJValue(data),
+          anchors = LookupExecutor.shortestPathAsJValue(FakeFoundFinder.simplePath, FakeFoundFinder.blockchains)
+        )
         val expectedLookupJValue = LookupJsonSupport.ToJson[LookupResult](expectedLookup).get
         val expectedGenericResponse = JValueGenericResponse.Success("Query Successfully Processed", expectedLookupJValue)
         val expectedGenericResponseAsJson = LookupJsonSupport.ToJson[JValueGenericResponse](expectedGenericResponse).toString
@@ -441,8 +539,21 @@ class LookupSpec extends TestBase with EmbeddedCassandra with LazyLogging {
         val value = "5aTelLQBerVT/vJiL2qjZCxWxqlfwT/BaID0zUVy7LyUC9nUdb02//aCiZ7xH1HglDqZ0Qqb7GyzF4jtBxfSBg=="
         val queryType = Signature
         val queryDepth = ShortestPath
+        val responseForm = AnchorsWithPath
+        val blockchainInfo = Normal
 
-        val pr = ProducerRecordHelper.toRecord(messageEnvelopeTopic, key, value, Map(QueryType.HEADER -> queryType.value, QueryDepth.HEADER -> queryDepth.value))
+        val pr = ProducerRecordHelper.toRecord(
+          messageEnvelopeTopic,
+          key,
+          value,
+          Map(
+            QueryType.HEADER -> queryType.value,
+            QueryDepth.HEADER -> queryDepth.value,
+            ResponseForm.HEADER -> responseForm.value,
+            BlockchainInfo.HEADER -> blockchainInfo.value
+          )
+        )
+
         publishToKafka(pr)
 
         //Consumer
@@ -636,7 +747,20 @@ class LookupSpec extends TestBase with EmbeddedCassandra with LazyLogging {
         val value = pmId
         val queryType = Payload
         val queryDepth = ShortestPath
-        val pr = ProducerRecordHelper.toRecord(messageEnvelopeTopic, key, value, Map(QueryType.HEADER -> queryType.value, QueryDepth.HEADER -> queryDepth.value))
+        val responseForm = AnchorsWithPath
+        val blockchainInfo = Normal
+
+        val pr = ProducerRecordHelper.toRecord(
+          messageEnvelopeTopic,
+          key,
+          value,
+          Map(
+            QueryType.HEADER -> queryType.value,
+            QueryDepth.HEADER -> queryDepth.value,
+            ResponseForm.HEADER -> responseForm.value,
+            BlockchainInfo.HEADER -> blockchainInfo.value
+          )
+        )
         publishToKafka(pr)
 
         //Consumer
@@ -657,6 +781,110 @@ class LookupSpec extends TestBase with EmbeddedCassandra with LazyLogging {
 
         assert(expectedGenericResponseAsJson == readMessage)
         assert(masterTree.category == masterTree.lookupKeys.headOption.map(_.category).getOrElse("No Cat"))
+
+      }
+
+    }
+
+    "consume and process successfully when Found with UpperLower" in {
+
+      cassandra.executeScripts(
+        CqlScript.statements(
+          insertEventSql
+        )
+      )
+
+      implicit val kafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(kafkaPort = PortGiver.giveMeKafkaPort, zooKeeperPort = PortGiver.giveMeZookeeperPort)
+
+      val bootstrapServers = "localhost:" + kafkaConfig.kafkaPort
+
+      val modules: List[Module] = List {
+        new LookupServiceBinder {
+
+          override def gremlin: ScopedBindingBuilder = bind(classOf[Gremlin]).to(classOf[DefaultTestingGremlinConnector])
+
+          override def finder: ScopedBindingBuilder = bind(classOf[Finder]).to(classOf[FakeFoundFinder])
+
+          override def config: ScopedBindingBuilder = bind(classOf[Config]).toProvider(new ConfigProvider {
+            override def conf: Config = {
+              super.conf
+                .withValue(
+                  "eventLog.kafkaConsumer.bootstrapServers",
+                  ConfigValueFactory.fromAnyRef(bootstrapServers)
+                )
+                .withValue(
+                  "eventLog.kafkaProducer.bootstrapServers",
+                  ConfigValueFactory.fromAnyRef(bootstrapServers)
+                )
+            }
+          })
+        }
+      }
+
+      val injector = new InjectorHelper(modules) {}
+
+      withRunningKafka {
+
+        val messageEnvelopeTopic = "com.ubirch.eventlog.lookup_request"
+        val eventLogTopic = "com.ubirch.eventlog.lookup_response"
+
+        val key = UUIDHelper.randomUUID.toString
+        val value = "c29tZSBieXRlcyEAAQIDnw=="
+        val queryType = Payload
+        val queryDepth = UpperLower
+        val responseForm = AnchorsWithPath
+        val blockchainInfo = Normal
+
+        val pr = ProducerRecordHelper.toRecord(
+          messageEnvelopeTopic,
+          key,
+          value,
+          Map(
+            QueryType.HEADER -> queryType.value,
+            QueryDepth.HEADER -> queryDepth.value,
+            ResponseForm.HEADER -> responseForm.value,
+            BlockchainInfo.HEADER -> blockchainInfo.value
+          )
+        )
+        publishToKafka(pr)
+
+        //Consumer
+        val consumer = injector.get[StringConsumer]
+        consumer.setTopics(Set(messageEnvelopeTopic))
+
+        consumer.startPolling()
+        //Consumer
+
+        Thread.sleep(5000)
+
+        val readMessage = consumeFirstStringMessageFrom(eventLogTopic)
+
+        val data =
+          """
+            |{
+            |   "hint":0,
+            |   "payload":"c29tZSBieXRlcyEAAQIDnw==",
+            |   "signature":"5aTelLQBerVT/vJiL2qjZCxWxqlfwT/BaID0zUVy7LyUC9nUdb02//aCiZ7xH1HglDqZ0Qqb7GyzF4jtBxfSBg==",
+            |   "signed":"lRKwjni1ymWXEeiBhcg+pwAOTQCwc29tZSBieXRlcyEAAQIDnw==",
+            |   "uuid":"8e78b5ca-6597-11e8-8185-c83ea7000e4d",
+            |   "version":34
+            |}
+          """.stripMargin
+
+        val expectedLookup = LookupResult.Found(
+          key = key,
+          queryType = queryType,
+          event = LookupJsonSupport.getJValue(data),
+          anchors = LookupExecutor.upperAndLowerAsJValue(FakeFoundFinder.upperPath, FakeFoundFinder.upperBlockchains, FakeFoundFinder.lowerPath, FakeFoundFinder.lowerBlockchains)
+        )
+        val expectedLookupJValue = LookupJsonSupport.ToJson[LookupResult](expectedLookup).get
+        val expectedGenericResponse = JValueGenericResponse.Success("Query Successfully Processed", expectedLookupJValue)
+        val expectedGenericResponseAsJson = LookupJsonSupport.ToJson[JValueGenericResponse](expectedGenericResponse).toString
+
+        println("R:" + readMessage)
+        println("E:" + expectedGenericResponseAsJson)
+
+        assert(expectedGenericResponseAsJson == readMessage)
 
       }
 
