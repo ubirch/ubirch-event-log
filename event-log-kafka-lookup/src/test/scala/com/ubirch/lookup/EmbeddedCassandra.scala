646f1977cff9baa541239926c18e2d5f5c074786
Carlos Sanchez <caaugustoss@gmail.com>
2019-12-02 17:42:37 +0100

    UP-1152 Testing wait problem when consuming - lookup 5

--- event-log-kafka-lookup/src/test/scala/com/ubirch/lookup/EmbeddedCassandra.scala
+++ event-log-kafka-lookup/src/test/scala/com/ubirch/lookup/EmbeddedCassandra.scala
@@ -2,10 +2,13 @@ package com.ubirch.lookup
 
 import com.github.nosan.embedded.cassandra.local.LocalCassandraFactoryBuilder
 import com.github.nosan.embedded.cassandra.test.TestCassandra
+import com.typesafe.scalalogging.LazyLogging
 
-trait EmbeddedCassandra {
+trait EmbeddedCassandra extends LazyLogging {
 
-  val factory = new LocalCassandraFactoryBuilder().build
+  val factory = new LocalCassandraFactoryBuilder().setJvmOptions("-Xms512m -Xmx1024m").build
+
+  logger.info("CASSANDRA CONFIG: " + factory.getJvmOptions.toString)
 
   val cassandra = new TestCassandra(factory)
 
