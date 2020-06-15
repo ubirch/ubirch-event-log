5ce373c5520b4af7611b80449dd826d5d556c193
Carlos Sanchez <caaugustoss@gmail.com>
2020-06-15 17:01:22 +0200

    Fixing test

--- event-log-encoder/src/test/scala/com/ubirch/encoder/EncoderSpec.scala
+++ event-log-encoder/src/test/scala/com/ubirch/encoder/EncoderSpec.scala
@@ -632,7 +632,7 @@ class EncoderSpec extends TestBase with LazyLogging {
         val publicKey =
           """
             |{
-            |  "hardware_id": "51f6cfe400bd1062f8fcde5dc5c23aaac111e8124886ecf1f60c33015a35ccb0",
+            |  "id": "51f6cfe400bd1062f8fcde5dc5c23aaac111e8124886ecf1f60c33015a35ccb0",
             |  "public_key": "e392457bdd63db37d00435bfdc0a0a7f4a85f3664b9439956a4f4f2310fd934df85ea4a02823d4674c891f224bcab8c8f2c117fdc8710ce78c928fc9de8d9e19"
             |}
           """.stripMargin
