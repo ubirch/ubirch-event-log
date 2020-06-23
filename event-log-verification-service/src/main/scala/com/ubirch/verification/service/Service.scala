ad2bbc147acc5fe1016376fc00b90299b4a2a463
Carlos Sanchez <caaugustoss@gmail.com>
2020-06-23 14:40:00 +0200

    Starting server

--- event-log-verification-service/src/main/scala/com/ubirch/verification/service/Service.scala
+++ event-log-verification-service/src/main/scala/com/ubirch/verification/service/Service.scala
@@ -1,13 +1,15 @@
 package com.ubirch.verification.service
 
 import com.ubirch.util.Boot
+import com.ubirch.verification.service.util.udash.JettyServer
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 object Service extends Boot(LookupServiceBinder.modules) {
 
   def main(args: Array[String]): Unit = {
-    implicit val globalExec: ExecutionContextExecutor = ExecutionContext.global
+
+    get[JettyServer]
 
   }
 
