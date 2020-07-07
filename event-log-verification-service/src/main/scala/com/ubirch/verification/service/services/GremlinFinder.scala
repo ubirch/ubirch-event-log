28af8d8c9b8107e776f010910219fe964cf505fb
sharkgua <benoit.george@ubirch.com>
2020-07-07 16:37:13 +0200

    Made gremlin queries better -> 2.5x faster

--- event-log-verification-service/src/main/scala/com/ubirch/verification/service/services/GremlinFinder.scala
+++ event-log-verification-service/src/main/scala/com/ubirch/verification/service/services/GremlinFinder.scala
@@ -23,9 +23,9 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
   def findUpperAndLowerAsVertices(id: String): Future[(List[VertexStruct], List[VertexStruct], List[VertexStruct], List[VertexStruct])] =
     for {
       (shortest, upper, lowerPath, lower) <- findUpperAndLower(id)
-      (completePathShortest, completeBlockchainsUpper) <- asVerticesDecorated(shortest, upper)
-      (completePathLower, completeBlockchainsLower) <- asVerticesDecorated(lowerPath, lower)
     } yield {
+      val (completePathShortest, completeBlockchainsUpper) = asVerticesDecorated(shortest, upper)
+      val (completePathLower, completeBlockchainsLower) = asVerticesDecorated(lowerPath, lower)
       (completePathShortest, completeBlockchainsUpper, completePathLower, completeBlockchainsLower)
     }
 
@@ -35,28 +35,25 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
     * @param id Id of the vertex
     * @return A Future of 4 vertex List corresponding to the shortest path, upper path, lower path and ???
     */
-  def findUpperAndLower(id: String): Future[(List[Vertex], List[Vertex], List[Vertex], List[Vertex])] = {
+  def findUpperAndLower(id: String): Future[(List[VertexStruct], List[VertexStruct], List[VertexStruct], List[VertexStruct])] = {
     val futureShortestPath: Future[PathHelper] = shortestPathFromVertexToBlockchain(id).map(PathHelper)
     val headTimestamp: Future[Option[Long]] = futureShortestPath.map(_.headOption).flatMap {
       case Some(v) =>
-        getTimestampFromVertexAsDate(v)
-          .map(_.map(_.getTime))
-          .recoverWith {
-            case e: Exception =>
-              logger.warn("Couldn't parse as Date. Defaulting to Long ")
-              getTimestampFromVertexAsLong(v)
-          }
+        getTimestampFromVertexAsDate(v) match {
+          case Some(value) => Future.successful(Some(value.getTime))
+          case None => Future.successful(getTimestampFromVertexAsLong(v))
+        }
       case None => Future.successful(None)
     }
 
-    val maybeLastMasterAndTime: Future[Option[(Vertex, Long)]] = for {
+    val maybeLastMasterAndTime: Future[Option[(VertexStruct, Long)]] = for {
       time <- headTimestamp
       master <- futureShortestPath.map(_.reversedTailHeadOption)
     } yield {
       master.map(x => (x, time.getOrElse(-1L)))
     }
 
-    val upper: Future[List[Vertex]] = maybeLastMasterAndTime.flatMap {
+    val upper: Future[List[VertexStruct]] = maybeLastMasterAndTime.flatMap {
       case Some((v, _)) =>
         getBlockchainsFromMasterVertex(v)
       case None =>
@@ -68,7 +65,7 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
       case None => Future.successful(None)
     }
 
-    val lower: Future[List[Vertex]] = lowerPathHelper.flatMap {
+    val lower: Future[List[VertexStruct]] = lowerPathHelper.flatMap {
       case Some((ph, t)) =>
         ph.reversedHeadOption
           .map(x => getBlockchainsFromMasterVertex(x))
@@ -102,21 +99,30 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
       .value(Key[String](returnProperty.toLowerCase()))
       .promise()
 
-  def getTimestampFromVertexAsLong(vertex: Vertex): Future[Option[Long]] =
-    gremlin.g.V(vertex)
-      .value[Long](Values.TIMESTAMP)
-      .promise()
-      .map(_.headOption)
+  def getTimestampFromVertexAsLong(vertex: VertexStruct): Option[Long] =
+    vertex.properties.get("timestamp") match {
+      case Some(value) =>
+        value match {
+          case date: String => Some(date.toLong)
+          case _ => None
+        }
+      case None => None
+    }
 
-  def getTimestampFromVertexAsDate(vertex: Vertex): Future[Option[Date]] =
-    gremlin.g.V(vertex)
-      .value[Date](Values.TIMESTAMP)
-      .promise()
-      .map(_.headOption)
+  def getTimestampFromVertexAsDate(vertex: VertexStruct): Option[Date] =
+    vertex.properties.get("timestamp") match {
+      case Some(value) =>
+        value match {
+          case date: Date => Some(date)
+          case _ => None
+        }
+      case None => None
+    }
 
-  def outLT(master: Vertex, time: Long): Future[List[Vertex]] = {
+  def outLT(master: VertexStruct, time: Long): Future[List[VertexStruct]] = {
     val timestamp = Key[Date](Values.TIMESTAMP)
-    gremlin.g.V(master)
+    val date = new Date(time)
+    val res = gremlin.g.V(master.id)
       .repeat(
         _.out(
           Values.MASTER_TREE_CATEGORY + "->" + Values.SLAVE_TREE_CATEGORY,
@@ -126,85 +132,94 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
       )
       .until(
         _.inE(Values.PUBLIC_CHAIN_CATEGORY + "->" + Values.MASTER_TREE_CATEGORY) // using vertex centered index
-          .has(timestamp, P.lt(new Date(time))) // Other P values like P.gt
+          .has(timestamp, P.lt(date)) // Other P values like P.gt
       )
-      .path()
       .limit(1)
+      .path()
       .unfold[Vertex]()
+      .elementMap
       .promise()
+    for {
+      vertices <- res
+    } yield {
+      vertices.map(v => VertexStruct.fromMap(v))
+    }
   }
 
-  def shortestPathFromVertexToBlockchain(hash: String): Future[List[Vertex]] = shortestPath(Values.HASH, hash, Values.PUBLIC_CHAIN_CATEGORY)
+  def shortestPathFromVertexToBlockchain(hash: String): Future[List[VertexStruct]] = shortestPath(Values.HASH, hash, Values.PUBLIC_CHAIN_CATEGORY)
 
-  def shortestPath(property: String, value: String, untilLabel: String): Future[List[Vertex]] = {
-    val x = StepLabel[java.util.Set[Vertex]]("x")
+  def shortestPath(property: String, value: String, untilLabel: String): Future[List[VertexStruct]] = {
+    //val x = StepLabel[java.util.Set[Vertex]]("x")
     //g.V().has("hash", hash).store("x").repeat(__.in().where(without("x")).aggregate("x")).until(hasLabel("PUBLIC_CHAIN")).limit(1).path().profile()
-    gremlin.g.V()
+    val shortestPath = gremlin.g.V()
       .has(Key[String](property.toLowerCase()), value)
-      .store(x)
-      .repeat(_.in().where(P.without[String](List("x"))).aggregate(x))
+      //.store(x)
+      .repeat(_.in().simplePath())
       .until(_.hasLabel(untilLabel))
       .limit(1)
       .path()
       .unfold[Vertex]()
+      .elementMap
       .promise()
+    for {
+      sp <- shortestPath
+    } yield {
+      sp.map(v => {
+        VertexStruct.fromMap(v)
+      })
+    }
   }
 
-  def asVertices(path: List[Vertex], anchors: List[Vertex]): Future[(List[VertexStruct], List[VertexStruct])] = {
+  def asVertices(path: List[VertexStruct], anchors: List[VertexStruct]): (List[VertexStruct], List[VertexStruct]) = {
 
     def withPrevious(hashes: List[Any]) = Map(Values.PREV_HASH -> hashes.mkString(","))
 
     def withNext(hashes: List[Any]) = Map(Values.NEXT_HASH -> hashes.mkString(","))
 
-    for {
-      pathV <- toVertexStruct(path)
-      blockchainsV <- toVertexStruct(anchors)
-    } yield {
+    val pathLinks =
+      path
+        .foldLeft(List.empty[VertexStruct]) { (acc, current) =>
 
-      val pathLinks =
-        pathV
-          .foldLeft(List.empty[VertexStruct]) { (acc, current) =>
+          val next = acc
+            .reverse
+            .headOption
+            .map(_.properties)
+            .flatMap(_.get(Values.HASH))
+            .map(x => withPrevious(List(x)))
+            .getOrElse(withPrevious(List("")))
 
-            val next = acc
-              .reverse
-              .headOption
-              .map(_.properties)
-              .flatMap(_.get(Values.HASH))
-              .map(x => withPrevious(List(x)))
-              .getOrElse(withPrevious(List("")))
+          acc ++ List(current.addProperties(next))
+        }
+        .foldRight(List.empty[VertexStruct]) { (current, acc) =>
 
-            acc ++ List(current.addProperties(next))
-          }
-          .foldRight(List.empty[VertexStruct]) { (current, acc) =>
+          val next = acc
+            .headOption
+            .map(_.properties)
+            .flatMap(_.get(Values.HASH))
+            .map(x => withNext(List(x)))
+            .getOrElse(withNext(List("")))
 
-            val next = acc
-              .headOption
-              .map(_.properties)
-              .flatMap(_.get(Values.HASH))
-              .map(x => withNext(List(x)))
-              .getOrElse(withNext(List("")))
+          current.addProperties(next) +: acc
+        }
 
-            current.addProperties(next) +: acc
-          }
+    val blockchainHashes = anchors.map(_.properties.getOrElse(Values.HASH, ""))
+    val upToMaster = pathLinks.reverse
+    val lastMaster = upToMaster.headOption.map(_.addProperties(withNext(blockchainHashes))).toList
+    val lastMasterHash = lastMaster.map(x => x.properties.getOrElse(Values.HASH, ""))
 
-      val blockchainHashes = blockchainsV.map(_.properties.getOrElse(Values.HASH, ""))
-      val upToMaster = pathLinks.reverse
-      val lastMaster = upToMaster.headOption.map(_.addProperties(withNext(blockchainHashes))).toList
-      val lastMasterHash = lastMaster.map(x => x.properties.getOrElse(Values.HASH, ""))
+    val completePath = {
+      val cp = if (upToMaster.isEmpty) Nil
+      else upToMaster.tail.reverse
 
-      val completePath = {
-        val cp = if (upToMaster.isEmpty) Nil
-        else upToMaster.tail.reverse
-
-        cp ++ lastMaster
-      }
-      val completeBlockchains = blockchainsV.map(_.addProperties(withPrevious(lastMasterHash)))
-
-      (completePath, completeBlockchains)
+      cp ++ lastMaster
     }
+    val completeBlockchains = anchors.map(_.addProperties(withPrevious(lastMasterHash)))
+
+    (completePath, completeBlockchains)
+
   }
 
-  def asVerticesDecorated(path: List[Vertex], anchors: List[Vertex]): Future[(List[VertexStruct], List[VertexStruct])] = {
+  def asVerticesDecorated(path: List[VertexStruct], anchors: List[VertexStruct]): (List[VertexStruct], List[VertexStruct]) = {
     def parseTimestamp(anyTime: Any): String = {
       anyTime match {
         case time if anyTime.isInstanceOf[Long] =>
@@ -225,7 +240,7 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
       }
     }
 
-    asVertices(path, anchors).map { case (p, a) =>
+    asVertices(path, anchors) match { case (p, a) =>
       val _path = p.map(x =>
         x.map(Values.TIMESTAMP)(parseTimestamp)
           .addLabelWhen(Values.FOUNDATION_TREE_CATEGORY)(Values.SLAVE_TREE_CATEGORY))
@@ -236,45 +251,14 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
     }
   }
 
-  def toVertexStruct(vertices: List[Vertex]): Future[List[VertexStruct]] = {
-    val futureRes = vertices.map { v =>
-      val gremlinRes = for {
-        jmaps <- gremlin.g.V(v).valueMap().promise().map(_.headOption)
-        label <- gremlin.g.V(v).label().promise().map(_.headOption)
-      } yield {
-        val maps = jmaps
-          .map(_.asScala.toMap)
-          .map(_.map { x =>
-            try {
-              val key = x._1.toString
-              val value = x._2.asInstanceOf[java.util.ArrayList[Any]].asScala.headOption.getOrElse("NO VALUE")
-              key -> value
-            } catch {
-              case e: Exception =>
-                logger.error("Error creating VertexStruct")
-                throw e
-            }
-          })
-          .getOrElse(Map.empty[String, String])
-        label.map(x => (x, maps))
-      }
-      gremlinRes
-    }
-
-    Future.sequence(futureRes)
-      .map { xs => xs.flatMap(y => y.toList) }
-      .map { xs => xs.map { case (a, b) => VertexStruct(a, b) } }
-  }
-
   def findAnchorsWithPathAsVertices(id: String): Future[(List[VertexStruct], List[VertexStruct])] =
     for {
       (path, anchors) <- findAnchorsWithPath(id)
-      (completePath, completeBlockchains) <- asVerticesDecorated(path, anchors)
     } yield {
-      (completePath, completeBlockchains)
+      asVerticesDecorated(path, anchors)
     }
 
-  def findAnchorsWithPath(id: String): Future[(List[Vertex], List[Vertex])] = {
+  def findAnchorsWithPath(id: String): Future[(List[VertexStruct], List[VertexStruct])] = {
     val futureShortestPath: Future[PathHelper] = shortestPathFromVertexToBlockchain(id).map(PathHelper)
 
     val maybeBlockchains = futureShortestPath.map(_.reversedTailHeadOption).flatMap {
@@ -289,29 +273,29 @@ class GremlinFinder @Inject() (gremlin: Gremlin)(implicit ec: ExecutionContext) 
 
   }
 
-  def getBlockchainsFromMasterVertexFromOption(master: Option[Vertex]): Future[List[Vertex]] =
-    master match {
-      case Some(value) => getBlockchainsFromMasterVertex(value)
-      case None => Future.successful(Nil)
-    }
-
-  def getBlockchainsFromMasterVertex(master: Vertex): Future[List[Vertex]] =
-    gremlin.g.V(master)
+  def getBlockchainsFromMasterVertex(master: VertexStruct): Future[List[VertexStruct]] = {
+    val futureBlockchains = gremlin.g.V(master.id)
       .in()
       .hasLabel(Values.PUBLIC_CHAIN_CATEGORY)
-      //.hasLabel(Values.PUBLIC_CHAIN_CATEGORY)
+      .elementMap
       .promise()
+    for {
+      blockchains <- futureBlockchains
+    } yield {
+      blockchains.map(b => VertexStruct.fromMap(b))
+    }
+  }
 
-  case class PathHelper(path: List[Vertex]) {
-    lazy val reversed: Seq[Vertex] = path.reverse
-    lazy val headOption: Option[Vertex] = path.headOption
-    lazy val reversedHeadOption: Option[Vertex] = reversed.headOption
-    lazy val reversedTail: Seq[Vertex] =
+  case class PathHelper(path: List[VertexStruct]) {
+    lazy val reversed: Seq[VertexStruct] = path.reverse
+    lazy val headOption: Option[VertexStruct] = path.headOption
+    lazy val reversedHeadOption: Option[VertexStruct] = reversed.headOption
+    lazy val reversedTail: Seq[VertexStruct] =
       if (reversed.isEmpty) Nil
       else reversed.tail
 
-    lazy val reversedTailReversed: Seq[Vertex] = reversedTail.reverse
-    lazy val reversedTailHeadOption: Option[Vertex] = reversedTail.headOption
+    lazy val reversedTailReversed: Seq[VertexStruct] = reversedTail.reverse
+    lazy val reversedTailHeadOption: Option[VertexStruct] = reversedTail.headOption
   }
 
 }
