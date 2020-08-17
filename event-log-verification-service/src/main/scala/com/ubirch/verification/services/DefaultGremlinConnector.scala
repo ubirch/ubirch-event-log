719cccb1233a56d00dc5f0161397a284b28678f1
Carlos Sanchez <caaugustoss@gmail.com>
2020-08-17 18:00:01 +0200

    UP-1905 Exporting var for nio pool

--- event-log-verification-service/src/main/scala/com/ubirch/verification/services/DefaultGremlinConnector.scala
+++ event-log-verification-service/src/main/scala/com/ubirch/verification/services/DefaultGremlinConnector.scala
@@ -15,12 +15,14 @@ import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory
 import scala.concurrent.Future
 
 trait GremlinConnectorPaths {
-  val HOSTS = "eventLog.gremlin.hosts"
-  val PORTS = "eventLog.gremlin.port"
-  val CLASS_NAME = "eventLog.gremlin.serializer.className"
-  val MAX_WAIT_FOR_CONNECTION = "eventLog.gremlin.connectionPool.maxWaitForConnection"
-  val RECONNECT_INTERVAL = "eventLog.gremlin.connectionPool.reconnectInterval"
-  val IO_REGISTRIES = "eventLog.gremlin.serializer.config.ioRegistries"
+  val HOSTS_PATH = "eventLog.gremlin.hosts"
+  val PORTS_PATH = "eventLog.gremlin.port"
+  val CLASS_NAME_PATH = "eventLog.gremlin.serializer.className"
+  val MAX_WAIT_FOR_CONNECTION_PATH = "eventLog.gremlin.connectionPool.maxWaitForConnection"
+  val RECONNECT_INTERVAL_PATH = "eventLog.gremlin.connectionPool.reconnectInterval"
+  val IO_REGISTRIES_PATH = "eventLog.gremlin.serializer.config.ioRegistries"
+  val NIO_POOL_SIZE_PATH = "eventLog.gremlin.nioPoolSize"
+  val WORKER_POOL_SIZE_PATH = "eventLog.gremlin.workerPoolSize"
 }
 
 trait Gremlin {
@@ -48,15 +50,22 @@ class DefaultGremlinConnector @Inject() (lifecycle: Lifecycle, config: Config)
 
   def buildProperties(config: Config): PropertiesConfiguration = {
     val conf = new PropertiesConfiguration()
-    conf.addProperty("hosts", config.getString(HOSTS))
-    conf.addProperty("port", config.getString(PORTS))
-    conf.addProperty("serializer.className", config.getString(CLASS_NAME))
-    conf.addProperty("connectionPool.maxWaitForConnection", config.getString(MAX_WAIT_FOR_CONNECTION))
-    conf.addProperty("connectionPool.reconnectInterval", config.getString(RECONNECT_INTERVAL))
+    conf.addProperty("hosts", config.getString(HOSTS_PATH))
+    conf.addProperty("port", config.getString(PORTS_PATH))
+    conf.addProperty("serializer.className", config.getString(CLASS_NAME_PATH))
+    conf.addProperty("connectionPool.maxWaitForConnection", config.getString(MAX_WAIT_FOR_CONNECTION_PATH))
+    conf.addProperty("connectionPool.reconnectInterval", config.getString(RECONNECT_INTERVAL_PATH))
     // no idea why the following line needs to be duplicated. Doesn't work without
     // cf https://stackoverflow.com/questions/45673861/how-can-i-remotely-connect-to-a-janusgraph-server first answer, second comment ¯\_ツ_/¯
-    conf.addProperty("serializer.config.ioRegistries", config.getAnyRef(IO_REGISTRIES).asInstanceOf[java.util.ArrayList[String]])
-    conf.addProperty("serializer.config.ioRegistries", config.getStringList(IO_REGISTRIES))
+    conf.addProperty("serializer.config.ioRegistries", config.getAnyRef(IO_REGISTRIES_PATH).asInstanceOf[java.util.ArrayList[String]])
+    conf.addProperty("serializer.config.ioRegistries", config.getStringList(IO_REGISTRIES_PATH))
+
+    val nioPoolSize = config.getInt(NIO_POOL_SIZE_PATH)
+    if (nioPoolSize > 0) conf.addProperty("nioPoolSize", nioPoolSize)
+
+    val workerPoolSize = config.getInt(WORKER_POOL_SIZE_PATH)
+    if (workerPoolSize > 0) conf.addProperty("workerPoolSize", workerPoolSize)
+
     conf
   }
 
