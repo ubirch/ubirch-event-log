e6226f2a9cba670daebf2b9640c366b01c16de0e
Carlos Sanchez <caaugustoss@gmail.com>
2020-10-12 18:20:15 +0200

    UNG-133 Refining token validation

--- event-log-verification-service/src/main/scala/com/ubirch/verification/models/Token.scala
+++ event-log-verification-service/src/main/scala/com/ubirch/verification/models/Token.scala
@@ -3,7 +3,7 @@ package com.ubirch.verification.models
 import java.security.PublicKey
 
 import com.typesafe.scalalogging.LazyLogging
-import com.ubirch.verification.util.Exceptions.InvalidOtherClaims
+import com.ubirch.verification.util.Exceptions.{ InvalidAllClaims, InvalidOtherClaims }
 import com.ubirch.verification.util.LookupJsonSupport
 import pdi.jwt.{ Jwt, JwtAlgorithm }
 
@@ -12,29 +12,40 @@ import scala.util.Try
 case class OtherClaims(role: Symbol)
 object OtherClaims {
 
-  def validate(otherClaims: OtherClaims): Boolean = otherClaims.role == 'verifier
+  def validate(otherClaims: OtherClaims): Option[OtherClaims] = {
+    if (otherClaims.role == 'verifier) {
+      Some(otherClaims)
+    } else {
+      None
+    }
+  }
 
 }
 
 object TokenVerification extends LazyLogging {
 
-  def decodeAndVerify(jwt: String, publicKey: PublicKey): Boolean = {
+  def decodeAndVerify(jwt: String, publicKey: PublicKey): Option[(Map[String, String], OtherClaims)] = {
     (for {
       (_, p, _) <- Jwt.decodeRawAll(jwt, publicKey, Seq(JwtAlgorithm.ES256))
-      _ = println(p)
       otherClaims <- Try(LookupJsonSupport.FromString[OtherClaims](p).get)
         .recover { case e: Exception => throw InvalidOtherClaims(e.getMessage, jwt) }
 
+      all <- Try(LookupJsonSupport.FromString[Map[String, String]](p).get)
+        .recover { case e: Exception => throw InvalidAllClaims(e.getMessage, jwt) }
+
     } yield {
-      OtherClaims.validate(otherClaims)
+      OtherClaims.validate(otherClaims).map((all, _))
     }).recover {
+      case e: InvalidAllClaims =>
+        logger.error(s"invalid_token_all_claims=${e.getMessage}", e)
+        None
       case e: InvalidOtherClaims =>
         logger.error(s"invalid_token_other_claims=${e.getMessage}", e)
-        false
+        None
       case e: Exception =>
         logger.error(s"invalid_token=${e.getMessage}", e)
-        false
-    }.getOrElse(false)
+        None
+    }.getOrElse(None)
 
   }
 
