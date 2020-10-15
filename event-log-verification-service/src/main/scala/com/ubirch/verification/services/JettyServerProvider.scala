e4b7c43a3df2ebfe6ada50cedc75b8a4266a82b8
Carlos Sanchez <caaugustoss@gmail.com>
2020-10-15 16:49:20 +0200

    UNG-133 Organizing API traits // starting docker conf

--- event-log-verification-service/src/main/scala/com/ubirch/verification/services/JettyServerProvider.scala
+++ event-log-verification-service/src/main/scala/com/ubirch/verification/services/JettyServerProvider.scala
@@ -2,13 +2,13 @@ package com.ubirch.verification.services
 
 import com.google.inject.Provider
 import com.typesafe.config.Config
-import com.ubirch.verification.controllers.{ Api, ApiImpl }
+import com.ubirch.verification.controllers.{ Api, DefaultApi }
 import com.ubirch.verification.util.udash.JettyServer
 import io.udash.rest.openapi.{ Info, Server }
 import javax.inject._
 
 @Singleton
-class JettyServerProvider @Inject() (apiImpl: ApiImpl, config: Config) extends Provider[JettyServer] {
+class JettyServerProvider @Inject() (api: Api, config: Config) extends Provider[JettyServer] {
 
   private val verificationConfig = config.getConfig("verification")
 
@@ -21,7 +21,7 @@ class JettyServerProvider @Inject() (apiImpl: ApiImpl, config: Config) extends P
     servers = List(Server(verificationConfig.getString("swaggerBaseUrl")))
   )
 
-  private val jettyServer = new JettyServer(apiImpl, openApi, verificationConfig.getInt("http.port"))
+  private val jettyServer = new JettyServer(api, openApi, verificationConfig.getInt("http.port"))
 
   override def get(): JettyServer = jettyServer
 
