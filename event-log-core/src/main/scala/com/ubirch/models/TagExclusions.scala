884603cf136e2c22a19853df42b818cab8d22d96
Carlos Sanchez <caaugustoss@gmail.com>
2022-08-08 16:36:47 +0200

    UC-1349 adding docs for tag exclusion

--- event-log-core/src/main/scala/com/ubirch/models/TagExclusions.scala
+++ event-log-core/src/main/scala/com/ubirch/models/TagExclusions.scala
@@ -2,10 +2,10 @@ package com.ubirch.models
 
 trait TagExclusions {
 
-  def `tags-exclude:` = "tags-exclude:"
+  def `tags-exclude:`: String = "tags-exclude:"
 
-  def headerExcludeBlockChain: (String, String) = HeaderNames.DISPATCHER -> "tags-exclude:blockchain"
-  def headerExcludeAggregation: (String, String) = HeaderNames.DISPATCHER -> "tags-exclude:aggregation"
-  def headerExcludeStorage: (String, String) = HeaderNames.DISPATCHER -> "tags-exclude:storage"
+  def headerExcludeBlockChain: (String, String) = HeaderNames.DISPATCHER -> (`tags-exclude:` + ":blockchain")
+  def headerExcludeAggregation: (String, String) = HeaderNames.DISPATCHER -> (`tags-exclude:` + "aggregation")
+  def headerExcludeStorage: (String, String) = HeaderNames.DISPATCHER -> (`tags-exclude:` + "storage")
 
 }
